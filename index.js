f88e996e4f47f58c5318210d48fca32a1b492fc1
Ганиева Альбина Нажиповна <erfolg1996@mail.ru>
2017-10-14 21:33:01 +0500

    Случайно изменила

--- index.js
+++ index.js
@@ -1,289 +1,60 @@
 'use strict';
 
-/**
- * Сделано задание на звездочку
- * Реализовано оба метода и tryLater
- */
-exports.isStar = true;
-let week = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
-let index = [0, 24, 48, 72, 96, 120, 144];
+var robbery = require('./robbery');
 
-let timeZone;
-
-let intervalTime = {
-    from: undefined,
-    to: undefined
+var gangSchedule = {
+    Danny: [
+        { from: 'ПН 12:00+5', to: 'ПН 17:00+5' },
+        { from: 'ВТ 13:00+5', to: 'ВТ 16:00+5' }
+    ],
+    Rusty: [
+        { from: 'ПН 11:30+5', to: 'ПН 16:30+5' },
+        { from: 'ВТ 13:00+5', to: 'ВТ 16:00+5' }
+    ],
+    Linus: [
+        { from: 'ПН 09:00+3', to: 'ПН 14:00+3' },
+        { from: 'ПН 21:00+3', to: 'ВТ 09:30+3' },
+        { from: 'СР 09:30+3', to: 'СР 15:00+3' }
+    ]
 };
 
-let freeTime = {
-    Bank: [],
-    Danny: [],
-    Rusty: [],
-    Linus: []
+var bankWorkingHours = {
+    from: '10:00+5',
+    to: '18:00+5'
 };
 
-let arrayAllIntervals = [];
+// Время не существует
+var longMoment = robbery.getAppropriateMoment(gangSchedule, 121, bankWorkingHours);
 
-let arrayIntervals = [];
+// Выведется false и ""
+console.info(longMoment.exists());
+console.info(longMoment.format('Метим на %DD, старт в %HH:%MM!'));
 
-let interval = {
-    timeNoForm: undefined,
-    time: undefined,
-    duration: undefined,
-    dayWeek: undefined
-};
+// Время существует
+var moment = robbery.getAppropriateMoment(gangSchedule, 90, bankWorkingHours);
 
-let tryL = 0;
+// Выведется true и "Метим на ВТ, старт в 11:30!"
+console.info(moment.exists());
+console.info(moment.format('Метим на %DD, старт в %HH:%MM!'));
 
-/**
- * @param {Object} schedule – Расписание Банды
- * @param {Number} duration - Время на ограбление в минутах
- * @param {Object} workingHours – Время работы банка
- * @param {String} workingHours.from – Время открытия, например, "10:00+5"
- * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
- * @returns {Object}
- */
-exports.getAppropriateMoment = function (schedule, duration, workingHours) {
-    console.info(schedule, duration, workingHours);
-    freeTime.Bank = [];
-    freeTime.Danny = [];
-    freeTime.Rusty = [];
-    freeTime.Linus = [];
-    arrayIntervals = [];
-    arrayAllIntervals = [];
-    parseData(schedule, workingHours);
-    arrayAllIntervals.sort(function (a, b) {
+if (robbery.isStar) {
+    // Вернет true
+    moment.tryLater();
+    // "ВТ 16:00"
+    console.info(moment.format('%DD %HH:%MM'));
 
-        return a - b;
-    });
-    findInterval();
-    let intervals = selection(duration);
+    // Вернет true
+    moment.tryLater();
+    // "ВТ 16:30"
+    console.info(moment.format('%DD %HH:%MM'));
 
-    return {
+    // Вернет true
+    moment.tryLater();
+    // "СР 10:00"
+    console.info(moment.format('%DD %HH:%MM'));
 
-        /**
-         * Найдено ли время
-         * @returns {Boolean}
-         */
-        exists: function () {
-            for (let i = 0; i < arrayIntervals.length; i++) {
-                if (duration <= arrayIntervals[i].duration) {
-
-                    return true;
-                }
-            }
-
-            return false;
-        },
-
-        /**
-         * Возвращает отформатированную строку с часами для ограбления
-         * Например,
-         *   "Начинаем в %HH:%MM (%DD)" -> "Начинаем в 14:59 (СР)"
-         * @param {String} template
-         * @returns {String}
-         */
-        format: function (template) {
-            if (intervals.length > tryL) {
-                template = template.replace(/%DD/, intervals[tryL].time.dayWeek);
-                template = template.replace(/%HH/, intervals[tryL].time.hour);
-                template = template.replace(/%MM/, intervals[tryL].time.min);
-
-                return template;
-            }
-
-            return '';
-        },
-
-        /**
-         * Попробовать найти часы для ограбления позже [*]
-         * @star
-         * @returns {Boolean}
-         */
-        tryLater: function () {
-            tryL++;
-            for (let i = tryL - 1; i < intervals.length - 1; i++) {
-                let next = intervals[i].timeNoForm.to - intervals[i + 1].timeNoForm.from;
-                if (next >= 30) {
-
-                    return true;
-                }
-            }
-
-            return false;
-        }
-    };
-};
-
-function parseData(schedule, workingHours) {
-    let fromBank = parseTime(workingHours.from, 'bank');
-    let toBank = parseTime(workingHours.to, 'bank');
-    for (let i = 0; i < 3; i++) {
-        let workTime = Object.create(intervalTime);
-        workTime.from = fromBank + index[i] * 60;
-        workTime.to = toBank + index[i] * 60;
-        freeTime.Bank.push(workTime);
-    }
-    parseSchedule(schedule.Danny, 'Danny');
-    parseSchedule(schedule.Rusty, 'Rusty');
-    parseSchedule(schedule.Linus, 'Linus');
-}
-
-function parseTime(time, flag) {
-    let assHour = time.split(':');
-    let assMin = assHour[1].split('+');
-    let newTime = Number(assHour[0]) * 60 + Number(assMin[0]);
-    if (flag === 'bank') {
-        timeZone = Number(assMin[1]);
-    } else {
-        newTime += (timeZone - Number(assMin[1])) * 60;
-    }
-
-    return newTime;
-}
-
-function parseSchedule(schedulePeople, namePeople) {
-    for (let i = 0; i < schedulePeople.length; i++) {
-        let dayWeekFrom = schedulePeople[i].from.split(' ');
-        let dayWeekTo = schedulePeople[i].to.split(' ');
-        let timeFrom;
-        let timeTo;
-        timeFrom = parseTime(dayWeekFrom[1], '');
-        timeTo = parseTime(dayWeekTo[1], '');
-        timeFrom = timeWithWeek(timeFrom, dayWeekFrom[0]);
-        timeTo = timeWithWeek(timeTo, dayWeekTo[0]);
-        linkage(timeFrom, timeTo, namePeople);
-    }
-}
-
-function timeWithWeek(time, dayWeek) {
-    for (let i = 0; i < 7; i++) {
-        if (dayWeek === week[i]) {
-            time += index[i] * 60;
-        }
-    }
-    if (time >= 168 * 60) {
-        time = time - 168 * 60;
-    }
-    if (time < 0) {
-        time = (168 * 60) + time;
-    }
-
-    return time;
-}
-
-function linkage(timeFrom, timeTo, name) {
-    let intervalTimePeople = Object.create(intervalTime);
-    intervalTimePeople.from = timeFrom;
-    intervalTimePeople.to = timeTo;
-    freeTime[name].push(intervalTimePeople);
-    arrayAllIntervals.push(timeFrom);
-    arrayAllIntervals.push(timeTo);
-}
-
-function findInterval() {
-    for (let i = 0; i < freeTime.Bank.length; i++) {
-        let maxFrom = freeTime.Bank[i].from;
-        let minTo = freeTime.Bank[i].to;
-        let length = freeTime.Danny.length + freeTime.Rusty.length + freeTime.Linus.length;
-        for (let j = 0; j < length; j++) {
-            maxFrom = findAndaddInterval(maxFrom, minTo, freeTime, i);
-        }
-    }
-}
-
-function findAndaddInterval(maxFrom, minTo, freeTime, i){
-    let inter = Object.create(intervalTime);
-    inter = findOptInterval(maxFrom, minTo, freeTime.Danny, inter);
-    inter = findOptInterval(inter.from, inter.to, freeTime.Rusty, inter);
-    inter = findOptInterval(inter.from, inter.to, freeTime.Linus, inter);
-    if (inter.from !== undefined && (inter.to - inter.from) !== 0) {
-        let newInterval = Object.create(interval);
-        newInterval.timeNoForm = inter;
-        newInterval.dayWeek = i;
-        newInterval.duration = inter.to - inter.from;
-        newInterval.time = answer(inter, i);
-        arrayIntervals.push(newInterval);
-        maxFrom = findStartNext(inter.to);
-    }
-
-    return maxFrom;
-}
-//упростить
-function findOptInterval(left, right, freeTimePeople, intervalFind) {
-    if (left === undefined) {
-        return intervalFind;
-    }
-    let arg = {
-        freeTimePeople: undefined,
-        left: left,
-        right: right,
-        k: 0,
-        optFrom: right
-    };
-    for (let i = 0; i < freeTimePeople.length; i++) {
-        arg.freeTimePeople = freeTimePeople[i];
-        arg = conditionFind(arg);
-    }
-    if (arg.k === freeTimePeople.length) {
-        intervalFind.from = undefined;
-        intervalFind.to = undefined;
-    } else {
-        intervalFind.from = arg.left;
-        intervalFind.to = arg.right;
-    }
-
-    return intervalFind;
-}
-
-function conditionFind(arg) {
-    let timePeople = arg.freeTimePeople;
-    if (timePeople.to <= arg.left || timePeople.from >= arg.right) {
-        arg.k++;
-    }
-    if (arg.left <= timePeople.from && timePeople.from < arg.optFrom && timePeople.from < arg.right) {
-        arg.optFrom = timePeople.from;
-        arg.left = arg.optFrom;
-    }
-    if (arg.left < timePeople.to && timePeople.to < arg.right) {
-        arg.right = timePeople.to;
-    }
-
-    return arg;
-}
-
-function findStartNext(to) {
-    let i = 0;
-    while (i < arrayAllIntervals.length) {
-        if (arrayAllIntervals[i] > to) {
-            return arrayAllIntervals[i];
-        }
-        i++;
-    }
-
-    return Number.to;
-}
-
-function answer(inter, dayWeek) {
-    let parseInterval = {};
-    let min = inter.from % 60;
-    if (min < 10) {
-        min = '0' + min;
-    }
-    parseInterval.min = min;
-    parseInterval.hour = (inter.from - parseInterval.min - (index[dayWeek] * 60)) / 60;
-    parseInterval.dayWeek = week[dayWeek];
-
-    return parseInterval;
-}
-
-function selection(duration) {
-    let arr = [];
-    for (let i = 0; i < arrayIntervals.length; i++) {
-        if (duration <= arrayIntervals[i].duration) {
-            arr.push(arrayIntervals[i]);
-        }
-    }
-
-    return arr;
+    // Вернет false
+    moment.tryLater();
+    // "СР 10:00"
+    console.info(moment.format('%DD %HH:%MM'));
 }
