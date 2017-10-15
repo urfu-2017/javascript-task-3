36ff6c5372993ad13aaea97d93d3eeff68ba93c8
samstikhin <samstikhin96@yandex.com>
2017-10-16 01:14:52 +0500

    First time

--- robbery.js
+++ robbery.js
@@ -6,6 +6,100 @@
  */
 exports.isStar = true;
 
+exports.converter = function () {
+    var MinInHour = 60;
+    var MinInDay = 60 * 24;
+    var DaysToNum = { 'ПН': 0, 'ВТ': 1, 'СР': 2 };
+    var NumToDays = { 0: 'ПН', 1: 'ВТ', 2: 'СР' };
+
+    return {
+        toNumber: function (timeString) {
+            var parseDate = timeString.split(/ |:|\+/);
+            var day = parseDate[0];
+            var hour = parseInt(parseDate[1]);
+            var min = parseInt(parseDate[2]);
+            var offsetInHours = parseInt(parseDate[3]);
+            var number =
+                DaysToNum[day] * MinInDay +
+                hour * MinInHour + min -
+                offsetInHours * MinInHour;
+
+            return { number: number, offset: offsetInHours };
+        },
+
+        toTime: function (number, offset, template) {
+            number += offset * MinInHour;
+            var day = NumToDays[Math.floor(number / MinInDay)];
+            var hour = Math.floor((number % MinInDay) / MinInHour);
+            var min = (number % MinInDay) % MinInHour;
+            if (hour < 10) {
+                hour = '0' + hour.toString();
+            }
+            if (min < 10) {
+                min = '0' + min.toString();
+            }
+
+            var aa = template.replace('%DD', day);
+            aa = aa.replace('%HH', hour);
+            aa = aa.replace('%MM', min);
+            aa = aa.replace('%OO', offset);
+
+            return aa;
+        },
+
+        toNumberInterval: function (interval) {
+            return {
+                from: this.toNumber(interval.from).number,
+                to: this.toNumber(interval.to).number
+            };
+        },
+
+        toTimeInterval: function (interval, offset) {
+            return {
+                from: this.toTime(interval.from, offset, '%DD %HH:%MM+%OO'),
+                to: this.toTime(interval.to, offset, '%DD %HH:%MM+%OO')
+            };
+        }
+    };
+};
+
+exports.intervalComparator = function (interval1, interval2) {
+    return {
+        notIntersect: (interval1.to <= interval2.from || interval1.from >= interval2.to),
+        firstIn: (interval2.from <= interval1.from && interval1.to <= interval2.to)
+    };
+};
+
+exports.isGoodTimeForAllGuys = function (timeInterval, schedule) {
+    var answer = true;
+    var curInterval = exports.converter().toNumberInterval(timeInterval);
+    for (var guy in schedule) {
+        if (!schedule.hasOwnProperty(guy)) {
+            continue;
+        }
+        for (var i = 0; i < schedule[guy].length; ++i) {
+            var guyInterval = exports.converter().toNumberInterval(schedule[guy][i]);
+            var cmp = exports.intervalComparator(curInterval, guyInterval);
+            answer = answer && cmp.notIntersect;
+        }
+    }
+
+    return answer;
+};
+
+exports.isGoodTimeForBank = function (timeInterval, bankWorkingIntervals) {
+    var curInterval = exports.converter().toNumberInterval(timeInterval);
+    for (var i = 0; i < 3; ++i) {
+        var workInterval = exports.converter().toNumberInterval(bankWorkingIntervals[i]);
+        var cmp = exports.intervalComparator(curInterval, workInterval);
+        if (cmp.firstIn) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
 /**
  * @param {Object} schedule – Расписание Банды
  * @param {Number} duration - Время на ограбление в минутах
@@ -16,6 +110,14 @@ exports.isStar = true;
  */
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
     console.info(schedule, duration, workingHours);
+    var offset = exports.converter().toNumber('ПН ' + workingHours.from).offset;
+    var bankWorkingIntervals = [
+        { from: 'ПН ' + workingHours.from, to: 'ПН ' + workingHours.to },
+        { from: 'ВТ ' + workingHours.from, to: 'ВТ ' + workingHours.to },
+        { from: 'СР ' + workingHours.from, to: 'СР ' + workingHours.to }];
+    var globalStart = exports.converter().toNumber(bankWorkingIntervals[0].from).number;
+    var globalEnd = exports.converter().toNumber(bankWorkingIntervals[2].to).number;
+    var answer;
 
     return {
 
@@ -24,7 +126,12 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            return false;
+            var result = this.findGoodTime(globalStart, globalEnd);
+            if (result.exist) {
+                answer = result.answer;
+            }
+
+            return result.exist;
         },
 
         /**
@@ -35,7 +142,11 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            return template;
+            if (answer === undefined) {
+                return '';
+            }
+
+            return exports.converter().toTime(answer, offset, template);
         },
 
         /**
@@ -44,7 +155,26 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
-            return false;
+            var nextTryDuration = 30;
+            var result = this.findGoodTime(answer + nextTryDuration, globalEnd);
+            if (result.exist) {
+                answer = result.answer;
+            }
+
+            return result.exist;
+        },
+
+        findGoodTime: function (start, end) {
+            for (var timeNum = start; timeNum < end; ++timeNum) {
+                var timeInterval = exports.converter().toTimeInterval(
+                    { from: timeNum, to: timeNum + duration }, offset);
+                if (exports.isGoodTimeForAllGuys(timeInterval, schedule) &&
+                    exports.isGoodTimeForBank(timeInterval, bankWorkingIntervals)) {
+                    return { exist: true, answer: timeNum };
+                }
+            }
+
+            return { exist: false };
         }
     };
 };
