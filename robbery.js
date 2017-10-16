ae0f7c356a6dc984c931366c1ba4ca7cc61cc8b8
Di <mm.di@mail.ru>
2017-10-17 01:09:45 +0500

    промежуточная проверка

--- robbery.js
+++ robbery.js
@@ -5,8 +5,11 @@
  * Реализовано оба метода и tryLater
  */
 exports.isStar = true;
-const DAY_TO_INT = { 'ПН': 0, 'ВТ': 1, 'СР': 2, 'ЧТ': 3, 'ПТ': 4, 'СБ': 5, 'ВС': 6 };
 const TRY_LATER_TIME = 30;
+const DAY_TO_INT = { 'ПН': 0, 'ВТ': 1, 'СР': 2, 'ЧТ': 3, 'ПТ': 4, 'СБ': 5, 'ВС': 6 };
+const HOUR_AS_MINUTES = 60;
+const DAY_AS_HOURS = 24;
+const DAY_AS_MINUTES = DAY_AS_HOURS * HOUR_AS_MINUTES;
 const ROBBERY_START_DAY = 'ПН';
 const ROBBERY_END_DAY = 'СР';
 
@@ -21,17 +24,26 @@ const ROBBERY_END_DAY = 'СР';
 
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
     console.info(schedule, duration, workingHours);
-    const data = {};
+
+    const robberyData = {};
     const robberyInterval = { from: ROBBERY_START_DAY, to: ROBBERY_END_DAY };
 
-    const [robberyIntervals, timezone] = getPossibleRobberyIntervals(workingHours, robberyInterval);
+    const [robberyIntervals, timezone] = getPossibleIntervals(workingHours, robberyInterval);
     const thiefBusyIntervals = getThiefBusyIntervals(schedule, timezone);
-    const disjointThiefIntervals = combineIntervals(thiefBusyIntervals);
-    const thiefReadinessIntervals = invertTimeIntervals(disjointThiefIntervals);
+    const thiefReadinessIntervals = invertTimeIntervals(thiefBusyIntervals);
 
-    data.possibleIntervals = getIntervalsIntersection(robberyIntervals, thiefReadinessIntervals);
-    data.satisfyingIntervals = data.possibleIntervals.filter(x=>x.to - x.from + 1 >= duration);
-    data.intervalIndex = data.satisfyingIntervals.findIndex(x=>x.to - x.from + 1 >= duration);
+    robberyData.possibleIntervals = getIntervalsIntersection(
+        robberyIntervals,
+        thiefReadinessIntervals
+    );
+
+    robberyData.satisfyingIntervals = robberyData
+        .possibleIntervals
+        .filter(x => x.to - x.from + 1 >= duration);
+
+    robberyData.intervalIndex = robberyData
+        .satisfyingIntervals
+        .findIndex(x => x.to - x.from + 1 >= duration);
 
     return {
 
@@ -40,7 +52,7 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            return data.satisfyingIntervals.length > 0;
+            return robberyData.satisfyingIntervals.length > 0;
         },
 
         /**
@@ -51,19 +63,17 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            const searchResult = data.satisfyingIntervals[data.intervalIndex];
+            const searchResult = robberyData.satisfyingIntervals[robberyData.intervalIndex];
             if (!searchResult) {
                 return '';
             }
-            const time = searchResult.from;
-            const days = Math.floor(time / (24 * 60));
-            const hours = Math.floor((time - days * 24 * 60) / 60);
-            const minutes = time - days * 24 * 60 - hours * 60;
+            const robberyStartTime = searchResult.from;
+            const [days, hours, minutes] = minutesToTime(robberyStartTime);
 
             return template
                 .replace(/%DD/, Object.keys(DAY_TO_INT).find(key => DAY_TO_INT[key] === days))
-                .replace(/%HH/, hours.toLocaleString(undefined, { minimumIntegerDigits: 2 }))
-                .replace(/%MM/, minutes.toLocaleString(undefined, { minimumIntegerDigits: 2 }));
+                .replace(/%HH/, ('0' + hours).slice(-2))
+                .replace(/%MM/, ('0' + minutes).slice(-2));
         },
 
         /**
@@ -72,25 +82,50 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
-            if (data.intervalIndex < 0) {
+            if (robberyData.intervalIndex < 0) {
                 return false;
             }
-            data.satisfyingIntervals[data.intervalIndex].from += TRY_LATER_TIME;
-            const nextItem = data.satisfyingIntervals.findIndex(x=>x.to - x.from + 1 >= duration);
+            robberyData.satisfyingIntervals[robberyData.intervalIndex].from += TRY_LATER_TIME;
+            const nextItem = robberyData
+                .satisfyingIntervals
+                .findIndex(x => x.to - x.from + 1 >= duration);
             if (nextItem >= 0) {
-                data.intervalIndex = nextItem;
+                robberyData.intervalIndex = nextItem;
 
                 return true;
             }
-            data.satisfyingIntervals[data.intervalIndex].from -= TRY_LATER_TIME;
+            robberyData.satisfyingIntervals[robberyData.intervalIndex].from -= TRY_LATER_TIME;
 
             return false;
         }
     };
 };
 
+/*
+class RobberyData {
+    constructor(schedule, duration, workingHours, robberyInterval) {
+        this.robberyData = {};
+        this.robberyInterval = robberyInterval;
+        this.workingHours = workingHours;
+        this.duration = duration;
+        this.schedule = schedule;
+    }
+
+    getRobberyIntervals() {
+const [intervals, timezone] = this.getPossibleIntervals(this.workingHours, this.robberyInterval);
+        const thiefBusyIntervals = this.getThiefBusyIntervals(this.schedule, timezone);
+        const thiefReadinessIntervals = invertTimeIntervals(thiefBusyIntervals);
+this.robberyData.possibleIntervals = getIntervalsIntersection(intervals, thiefReadinessIntervals);
+    }
+
+    find
+
+
+}*/
+
 /**
- * Возвращает интервалы, в которые возможно совершить ограбления банка
+ * Возвращает интервалы, в которые возможно совершить ограбления банка,
+ * и их временную зону
  * @param {Array} workingHours
  * @param {Object} robberyInterval - Пределы ограбления
  * @param {String} robberyInterval.from – Время открытия интервала ограбления
@@ -98,16 +133,16 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
  * @returns {Array} - интервалы ограбления банка
  * @returns {Number} - временная зона банка
  */
-function getPossibleRobberyIntervals(workingHours, robberyInterval) {
+function getPossibleIntervals(workingHours, robberyInterval) {
     let intervals = [];
+    const startDayNumber = DAY_TO_INT[robberyInterval.from];
+    const endDayNumber = DAY_TO_INT[robberyInterval.to];
     const [hoursFrom, minutesFrom, baseTimezone] = parseTime(workingHours.from);
     const [hoursTo, minutesTo] = parseTime(workingHours.to);
-    const startDay = robberyInterval.from;
-    const endDay = robberyInterval.to;
 
-    for (let dayNumber = DAY_TO_INT[startDay]; dayNumber <= DAY_TO_INT[endDay]; dayNumber++) {
-        const from = (hoursFrom + 24 * dayNumber) * 60 + minutesFrom;
-        const to = (hoursTo + 24 * dayNumber) * 60 + minutesTo - 1;
+    for (let dayNumber = startDayNumber; dayNumber <= endDayNumber; dayNumber++) {
+        const from = timeToMinutes(dayNumber, hoursFrom, minutesFrom);
+        const to = timeToMinutes(dayNumber, hoursTo, minutesTo - 1);
         intervals.push({ from, to });
     }
 
@@ -121,23 +156,33 @@ function getPossibleRobberyIntervals(workingHours, robberyInterval) {
  * @returns {Array} - интервалы занятости грабителей
  */
 function getThiefBusyIntervals(schedule, baseTimezone) {
-    let intervals = [];
-    for (let thiefIntervals in schedule) {
-        if (!schedule[thiefIntervals]) {
-            continue;
-        }
-        for (var interval of schedule[thiefIntervals]) {
+    let thiefBusyIntervals = Object.keys(schedule)
+        .map(name => schedule[name].map(function (interval) {
             const [weekDayNumberFrom, hoursFrom, minutesFrom, timezone] = parseTime(interval.from);
             const [weekDayNumberTo, hoursTo, minutesTo] = parseTime(interval.to);
             const timeDifference = baseTimezone - timezone;
-            const from = (hoursFrom + timeDifference + 24 * weekDayNumberFrom) * 60 + minutesFrom;
-            const to = (hoursTo + timeDifference + 24 * weekDayNumberTo) * 60 + minutesTo;
-            intervals.push({ from, to });
-        }
-    }
+            const from = timeToMinutes(weekDayNumberFrom, hoursFrom + timeDifference, minutesFrom);
+            const to = timeToMinutes(weekDayNumberTo, hoursTo + timeDifference, minutesTo);
 
-    return intervals;
+            return { from, to };
+        }))
+        .reduce((intervals, currentThiefIntervals) => intervals.concat(currentThiefIntervals));
+
+    return combineIntervals(thiefBusyIntervals);
 }
+
+function timeToMinutes(days, hours, minutes) {
+    return (days * DAY_AS_HOURS + hours) * HOUR_AS_MINUTES + minutes;
+}
+
+function minutesToTime(timeAsMinutes) {
+    const days = Math.floor(timeAsMinutes / (DAY_AS_MINUTES));
+    const hours = Math.floor((timeAsMinutes - days * DAY_AS_MINUTES) / HOUR_AS_MINUTES);
+    const minutes = timeAsMinutes - days * DAY_AS_MINUTES - hours * HOUR_AS_MINUTES;
+
+    return [days, hours, minutes];
+}
+
 
 /**
  * Склеивает пересекающиеся интервалы
@@ -150,8 +195,8 @@ function combineIntervals(intervals) {
     for (let i = 0; i < sortedIntervals.length; i++) {
         const current = sortedIntervals[i];
         const overlappingIntervals = sortedIntervals
-            .filter(x=> current.from <= x.from && x.from <= current.to + 1);
-        const intervalsBorder = Math.max.apply(null, overlappingIntervals.map((x)=>x.to));
+            .filter(x => current.from <= x.from && x.from <= current.to + 1);
+        const intervalsBorder = Math.max(...overlappingIntervals.map(x => x.to));
         combinedIntervals.push({ from: current.from, to: intervalsBorder });
         i += overlappingIntervals.length - 1;
     }
@@ -165,17 +210,14 @@ function combineIntervals(intervals) {
  * @returns {Array}
  */
 function invertTimeIntervals(intervals) {
-    const minValue = Number.MIN_SAFE_INTEGER;
-    const maxValue = Number.MAX_SAFE_INTEGER;
-    let invertedIntervals = [];
-    let previousEndPos = minValue;
-    for (let i = 0; i < intervals.length; i++) {
-        const interval = intervals[i];
+    let previousEndPos = -Infinity;
+    let invertedIntervals = intervals.map(function (interval) {
         const invertedInterval = { from: previousEndPos, to: interval.from - 1 };
         previousEndPos = interval.to;
-        invertedIntervals.push(invertedInterval);
-    }
-    invertedIntervals.push({ from: previousEndPos, to: maxValue });
+
+        return invertedInterval;
+    });
+    invertedIntervals.push({ from: previousEndPos, to: Infinity });
 
     return invertedIntervals;
 }
@@ -187,13 +229,11 @@ function invertTimeIntervals(intervals) {
  * @returns {Array}
  */
 function getIntervalsIntersection(bankIntervals, thiefIntervals) {
-    let intervalsIntersections = [];
-    for (let bankInterval of bankIntervals) {
-        const localIntersections = findIntersections(bankInterval, thiefIntervals);
-        intervalsIntersections = intervalsIntersections.concat(localIntersections);
-    }
+    let intervalIntersections = bankIntervals.reduce(function (intersections, bankInterval) {
+        return intersections.concat(findIntersections(bankInterval, thiefIntervals));
+    }, []);
 
-    return combineIntervals(intervalsIntersections);
+    return combineIntervals(intervalIntersections);
 }
 
 /**
