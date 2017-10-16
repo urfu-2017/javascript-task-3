da5a96fbe654ec0350a0f968efb1f9d6520aa841
aimaev <albert.voiceinyou@gmail.com>
2017-10-16 20:09:26 +0500

    try 1(no star)

--- robbery.js
+++ robbery.js
@@ -6,19 +6,23 @@
  */
 exports.isStar = true;
 const DAYS_OF_THE_WEEK = { 'ПН': 0, 'ВТ': 1, 'СР': 2, 'ЧТ': 3, 'ПТ': 4, 'СБ': 5, 'ВС': 6 };
-const NUMBER_TO_DAY = ['ПН', 'ВТ', 'СР', 'ЧТ'];
-const robbersTimeFormat = /^([А-Я][А-Я])\s(\d\d):(\d\d)\+(\d)$/;
-const bankTimeFormat = /^(\d\d):(\d\d)\+(\d)$/;
-const minutesInWeek = 7 * 24 * 60;
-const border = 24 * 60;
+const robbersTimeFormat = /^([А-Я][А-Я])\s(\d\d):(\d\d)\+(\d+)$/;
+const bankTimeFormat = /^(\d\d):(\d\d)\+(\d+)$/;
+const DAYS_IN_WEEK = 7;
+const HOURS_IN_DAY = 24;
+const MINUTES_IN_HOUR = 60;
+const MINUTES_IN_WEEK = DAYS_IN_WEEK * HOURS_IN_DAY * MINUTES_IN_HOUR;
+const END_OF_DAY = HOURS_IN_DAY * MINUTES_IN_HOUR;
 
 function getBankScheduleAndTimeZone(workingHours) {
     let [, hoursFrom, minutesFrom, zone] = workingHours.from.match(bankTimeFormat);
     let [, hoursTo, minutesTo] = workingHours.to.match(bankTimeFormat);
     let schedule = [];
     for (let i = 0; i < 3; i++) {
-        let fromInMinutes = (i * 24 + parseInt(hoursFrom)) * 60 + parseInt(minutesFrom);
-        let toInMinutes = (i * 24 + parseInt(hoursTo)) * 60 + parseInt(minutesTo);
+        let fromInMinutes = (i * HOURS_IN_DAY + parseInt(hoursFrom)) *
+            MINUTES_IN_HOUR + parseInt(minutesFrom);
+        let toInMinutes = (i * HOURS_IN_DAY + parseInt(hoursTo)) *
+            MINUTES_IN_HOUR + parseInt(minutesTo);
         schedule.push({ from: fromInMinutes, to: toInMinutes });
     }
 
@@ -28,8 +32,8 @@ function getBankScheduleAndTimeZone(workingHours) {
 function robberTimeToMinutes(time, bankTimeZone) {
     let [, dayOfWeek, hh, mm, zone] = time.match(robbersTimeFormat);
     let diff = bankTimeZone - zone;
-    let result = ((DAYS_OF_THE_WEEK[dayOfWeek] * 24 + parseInt(hh) +
-        parseInt(diff)) * 60) + parseInt(mm);
+    let result = ((DAYS_OF_THE_WEEK[dayOfWeek] * HOURS_IN_DAY + parseInt(hh) +
+        parseInt(diff)) * MINUTES_IN_HOUR) + parseInt(mm);
 
     return result;
 }
@@ -47,12 +51,12 @@ function getGeneralSchedule(schedule, bankTimeZone) {
     return generalSchedule;
 }
 
-function getTimeLine(bankSchedule, generalSchedule) {
-    let robbersTimeLine = [];
+function getTimeLines(bankSchedule, generalSchedule) {
     let bankTimeLine = [];
-    for (let i = 0; i < minutesInWeek; i++) {
-        robbersTimeLine.push(true);
+    let robbersTimeLine = [];
+    for (let i = 0; i < MINUTES_IN_WEEK; i++) {
         bankTimeLine.push(false);
+        robbersTimeLine.push(true);
     }
     for (let i = 0; i < bankSchedule.length; i++) {
         let time = bankSchedule[i];
@@ -66,8 +70,13 @@ function getTimeLine(bankSchedule, generalSchedule) {
             robbersTimeLine[j] = false;
         }
     }
+
+    return [bankTimeLine, robbersTimeLine];
+}
+
+function getTimeLine(bankTimeLine, robbersTimeLine) {
     let timeline = [];
-    for (let i = 0; i < minutesInWeek; i++) {
+    for (let i = 0; i < MINUTES_IN_WEEK; i++) {
         timeline.push(robbersTimeLine[i] && bankTimeLine[i]);
     }
 
@@ -78,32 +87,27 @@ function getIntervals(timeline, duration) {
     let intervals = [];
     let tempFrom = 0;
     let inInterval = false;
-    duration = parseInt(duration);
 
-    /* 
-    for (let i = 2100; i < 2300; i += 10) {
-        console.info(JSON.stringify(timeline.slice(i, i + 10)));
-    }*/
-
-    for (let i = 0; i < minutesInWeek; i++) {
-        if (timeline[i] && !inInterval) {
-            tempFrom = i;
+    timeline.forEach(function (element, i) {
+        if (element && !inInterval) {
             inInterval = true;
-        } else if (!timeline[i] && inInterval) {
+            tempFrom = i;
+        } else if (!element && inInterval) {
+            inInterval = false;
             if (i - tempFrom >= duration) {
                 intervals.push({ from: tempFrom, to: i });
             }
+        } else if (i !== 0 && (i % END_OF_DAY === 0)) {
             inInterval = false;
-        } else if (i !== 0 && (i % border === 0)) {
             if (i - tempFrom >= duration && inInterval) {
                 intervals.push({ from: tempFrom, to: i });
             }
-            inInterval = false;
         }
-    }
+    });
 
     return intervals;
 }
+
 
 /**
  * @param {Object} schedule – Расписание Банды
@@ -114,15 +118,11 @@ function getIntervals(timeline, duration) {
  * @returns {Object}
  */
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
-    // console.info(schedule, duration, workingHours);
     let [bankSchedule, zone] = getBankScheduleAndTimeZone(workingHours);
-    // console.info(JSON.stringify(bankSchedule));
-    // console.info(zone);
     let generalSchedule = getGeneralSchedule(schedule, zone);
-    console.info(JSON.stringify(generalSchedule));
-    let timeline = getTimeLine(bankSchedule, generalSchedule);
+    let [bankTimeLine, robbersTimeLine] = getTimeLines(bankSchedule, generalSchedule);
+    let timeline = getTimeLine(bankTimeLine, robbersTimeLine);
     let intervals = getIntervals(timeline, duration);
-    console.info(JSON.stringify(intervals));
 
     return {
 
@@ -146,9 +146,10 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
                 return '';
             }
             let startTimeInMinutes = intervals[0].from;
-            let mm = startTimeInMinutes % 60;
-            let hh = ((startTimeInMinutes - mm) / 60) % 24;
-            let dd = NUMBER_TO_DAY[Math.floor(((startTimeInMinutes - mm) / 60) / 24)];
+            let mm = startTimeInMinutes % MINUTES_IN_HOUR;
+            let hh = ((startTimeInMinutes - mm) / MINUTES_IN_HOUR) % HOURS_IN_DAY;
+            let index = Math.floor(((startTimeInMinutes - mm) / MINUTES_IN_HOUR) / HOURS_IN_DAY);
+            let dd = Object.keys(DAYS_OF_THE_WEEK)[index];
             mm = ('0' + mm).slice(-2);
             hh = ('0' + hh).slice(-2);
 
