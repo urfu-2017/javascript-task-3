cd6f6f83cb821ca307281034a15e9bcb6b8fc090
Yui Hirasawa <bosnianrainbo@gmail.com>
2017-10-16 09:48:38 +0500

    1

--- robbery.js
+++ robbery.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const weekDays = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+
 /**
  * Сделано задание на звездочку
  * Реализовано оба метода и tryLater
@@ -17,6 +19,24 @@ exports.isStar = true;
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
     console.info(schedule, duration, workingHours);
 
+    const addDay = ({ from, to }, day) => ({ from: `${day} ${from}`, to: `${day} ${to}` });
+    const parseAll = ({ from, to }) => ({ from: parseDate(from), to: parseDate(to) });
+    const convertAll = (timeZone, { from, to }) => ({
+        from: convertTimeZone(from, timeZone),
+        to: convertTimeZone(to, timeZone)
+    });
+
+    const bankWorkTime = ['ПН', 'ВТ', 'СР']
+        .map(addDay.bind(null, workingHours))
+        .map(parseAll);
+
+    const parsedSchedule = Object.keys(schedule)
+        .reduce((array, name) => array.concat(schedule[name]), [])
+        .map(parseAll)
+        .map(convertAll.bind(null, bankWorkTime[0].from.timeZone));
+
+    let result = findFrom(0, parsedSchedule, duration, bankWorkTime);
+
     return {
 
         /**
@@ -24,7 +44,7 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            return false;
+            return result.success;
         },
 
         /**
@@ -35,7 +55,23 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            return template;
+            if (!result.success) {
+                return '';
+            }
+
+            const timeInMinutes = result.time % (24 * 60);
+            const minutes = timeInMinutes % 60;
+            const day = parseInt((result.time - timeInMinutes) / (24 * 60));
+            const hours = parseInt((timeInMinutes - minutes) / 60);
+
+            const minsStr = minutes < 10 ? '0' + minutes.toString() : minutes;
+            const hoursStr = hours < 10 ? '0' + hours.toString() : hours;
+
+            let formatedStr = template.replace('%HH', hoursStr);
+            formatedStr = formatedStr.replace('%MM', minsStr);
+            formatedStr = formatedStr.replace('%DD', weekDays[day]);
+
+            return formatedStr;
         },
 
         /**
@@ -44,7 +80,65 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
-            return false;
+            if (!result.success) {
+                return false;
+            }
+
+            const newResult = findFrom(result.time + 30, parsedSchedule, duration, bankWorkTime);
+            if (newResult.success) {
+                result = newResult;
+            }
+
+            return newResult.success;
         }
     };
 };
+
+function findFrom(fromTime, schedule, duration, workingHours) {
+    let result = false;
+    let time = 0;
+
+    for (let i = fromTime; i < 24 * 60 * 3; i++) {
+        const isFreeTime = schedule.filter(({ from, to }) =>
+            (i > from.time && i < to.time) ||
+            (i + duration > from.time && i + duration < to.time)
+        ).length === 0;
+
+        const isWorkTime = workingHours.filter(({ from, to }) =>
+            i >= from.time && i + duration <= to.time
+        ).length >= 1;
+
+        if (isFreeTime && isWorkTime) {
+            time = i;
+            result = true;
+            break;
+        }
+    }
+
+    return {
+        success: result,
+        time: time
+    };
+}
+
+function convertTimeZone(time, timeZone) {
+    const timeDiff = timeZone - time.timeZone;
+
+    return {
+        time: time.time + 60 * timeDiff,
+        timeZone: timeZone
+    };
+}
+
+function parseDate(dateStr) {
+    const dateArray = dateStr.split(' ');
+    const fullTime = dateArray[1];
+    const [timeStr, timeZone] = fullTime.split('+');
+    const [hours, minutes] = timeStr.split(':').map(x => parseInt(x));
+    const dayIndex = weekDays.indexOf(dateArray[0]);
+
+    return {
+        time: hours * 60 + minutes + 24 * 60 * dayIndex,
+        timeZone: parseInt(timeZone)
+    };
+}
