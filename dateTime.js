3f1a343f9b24676da1956c444d3d1433c0f7e391
ArtyMgn <r.artem74@gmail.com>
2017-10-18 01:31:31 +0500

    first version

---
+++ dateTime.js
@@ -0,0 +1,93 @@
+'use strict';
+
+const DATETIME_PATTERN = /^(ПН|ВТ|СР|ЧТ|ВС)?\s?([0-1]?[0-9]|2[0-3]):([0-5][0-9])\+([0-9]|1[0-2])$/;
+const DAYS = ['ПН', 'ВТ', 'СР'];
+const MAX_MINUTE = 59;
+const MAX_HOUR = 23;
+
+exports.compareDatetimes = function (dateTime1, dateTime2) {
+    let indexOfDay1 = DAYS.indexOf(dateTime1.day);
+    let indexOfDay2 = DAYS.indexOf(dateTime2.day);
+
+    if (indexOfDay1 === indexOfDay2 && dateTime1.hours === dateTime2 &&
+        dateTime1.minutes === dateTime2.minutes) {
+        return 0;
+    }
+
+    return (indexOfDay1 < indexOfDay2 || (indexOfDay2 === indexOfDay1) &&
+        ((dateTime1.hours < dateTime2.hours) || (dateTime1.hours === dateTime2.hours) &&
+        (dateTime1.minutes < dateTime2.minutes))) ? -1 : 1;
+};
+
+exports.equals = function (dateTime1, dateTime2) {
+    return dateTime1.hours === dateTime2.hours &&
+    dateTime1.minutes === dateTime2.minutes &&
+    dateTime1.day === dateTime2.day;
+};
+
+exports.parse = function (datetime) {
+    let match = DATETIME_PATTERN.exec(datetime);
+
+    return {
+        day: match[1],
+        hours: Number(match[2]),
+        minutes: Number(match[3]),
+        timeZone: Number(match[4])
+    };
+};
+
+exports.getElapsedMinutes = function (startDatetime, endDatetime) {
+    let startDatetimeMinutes = exports.getElapsedMinutesSinceBeginOfWeek(startDatetime);
+    let endDatetimeMinutes = exports.getElapsedMinutesSinceBeginOfWeek(endDatetime);
+
+    return endDatetimeMinutes - startDatetimeMinutes;
+};
+
+exports.getElapsedMinutesSinceBeginOfWeek = function (dateTime) {
+    return 60 * (DAYS.indexOf(dateTime.day) * 24 + dateTime.hours) + dateTime.minutes;
+};
+
+exports.changeTimeZone = function (dateTime, newTimeZone) {
+    let offset = newTimeZone - dateTime.timeZone;
+    dateTime.hours += offset;
+    dateTime.timeZone = newTimeZone;
+
+    if (dateTime.hours > MAX_HOUR) {
+        moveOnOneDay(dateTime, 1);
+    }
+
+    if (dateTime.hours < 0) {
+        moveOnOneDay(dateTime, -1);
+    }
+};
+
+exports.addMinutes = function (dateTime, minutes) {
+    dateTime.minutes += minutes;
+    if (dateTime.minutes > MAX_MINUTE) {
+        dateTime.hours += 1;
+        dateTime.minutes = dateTime.minutes % MAX_MINUTE;
+    }
+
+    if (dateTime.hours > MAX_HOUR) {
+        dateTime.hours = dateTime.minutes % MAX_HOUR;
+        moveOnOneDay(dateTime, 1);
+    }
+};
+
+function moveOnOneDay(dateTime, direction) {
+    let indexOfCurrentDay = DAYS.indexOf(dateTime.day);
+    if (indexOfCurrentDay + direction < 0) {
+        dateTime.day = DAYS[0];
+        dateTime.hours = 0;
+        dateTime.minutes = 0;
+    } else
+    if (indexOfCurrentDay + direction >= DAYS.length) {
+        dateTime.day = DAYS[DAYS.length - 1];
+        dateTime.hours = MAX_HOUR;
+        dateTime.minutes = MAX_MINUTE;
+    } else {
+        dateTime.day = DAYS[indexOfCurrentDay + direction];
+        dateTime.hours = dateTime.hours - direction * 24;
+    }
+}
+
--- robbery.js
+++ robbery.js
@@ -5,6 +5,7 @@
  * Реализовано оба метода и tryLater
  */
 exports.isStar = true;
+var dateTime = require('./dateTime');
 
 /**
  * @param {Object} schedule – Расписание Банды
@@ -15,16 +16,25 @@ exports.isStar = true;
  * @returns {Object}
  */
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
-    console.info(schedule, duration, workingHours);
+    let banksWorkingHours = parseTimeInterval(workingHours);
+    let momentsWhenBankIsClosed = getMomentsWhenBankIsClosed(banksWorkingHours);
+    let busyMoments = getTimeIntervalsWithBankTimezone(schedule, banksWorkingHours.from.timeZone)
+        .concat(momentsWhenBankIsClosed)
+        .sort((x, y) => dateTime.compareDatetimes(x.from, y.from));
+
+    let appropriateMoments = getAppropriateMoments(unionTimeIntervals(busyMoments), duration);
 
     return {
+        robberyDuration: duration,
+        currentMomentIndex: 0,
+        moments: appropriateMoments,
 
         /**
          * Найдено ли время
          * @returns {Boolean}
          */
         exists: function () {
-            return false;
+            return this.moments.length !== 0;
         },
 
         /**
@@ -35,7 +45,14 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            return template;
+            if (appropriateMoments.length === 0) {
+                return '';
+            }
+
+            let robberyBeginning = this.moments[this.currentMomentIndex].from;
+
+            return template.replace(/%(?:(?:HH)|(?:DD)|(?:MM))/g,
+                match => replaceMatched(match, robberyBeginning));
         },
 
         /**
@@ -44,7 +61,105 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
+            let startTime = appropriateMoments[this.currentMomentIndex].from;
+
+            let minutesForNextAttempt = 30;
+            for (let i = this.currentMomentIndex; i < appropriateMoments.length; i++) {
+                let moment = appropriateMoments[i];
+                minutesForNextAttempt -= dateTime.getElapsedMinutes(startTime, moment.from);
+                let momentDuration = getMomentDurationInMinutes(moment);
+                if (momentDuration >= this.robberyDuration + minutesForNextAttempt) {
+                    this.currentMomentIndex = i;
+                    dateTime.addMinutes(moment.from,
+                        minutesForNextAttempt > 0 ? minutesForNextAttempt : 0);
+
+                    return true;
+                }
+            }
+
             return false;
         }
     };
 };
+
+function replaceMatched(match, momentTime) {
+    switch (match) {
+        case '%DD':
+            return momentTime.day;
+        case '%HH':
+            return momentTime.hours < 10 ? `0${momentTime.hours}` : momentTime.hours;
+        case '%MM':
+            return momentTime.minutes < 10 ? `0${momentTime.minutes}` : momentTime.minutes;
+        default:
+            return '';
+    }
+}
+
+function parseTimeInterval(timeInterval) {
+    return { from: dateTime.parse(timeInterval.from), to: dateTime.parse(timeInterval.to) };
+}
+
+function getTimeIntervalsWithBankTimezone(schedule, bankTimeZone) {
+    return Object.values(schedule).reduce((a, b) => a.concat(b))
+        .map(function (x) {
+            let timeInterval = parseTimeInterval(x);
+            Object.values(timeInterval).map(t => dateTime.changeTimeZone(t, bankTimeZone));
+
+            return timeInterval;
+        });
+}
+
+function getMomentsWhenBankIsClosed(workingHours) {
+    let moments = [];
+    for (let currentDay of ['ПН', 'ВТ', 'СР']) {
+        let timeBeforeOpening = { from: { day: currentDay, hours: 0, minutes: 0 },
+            to: { day: currentDay, hours: workingHours.from.hours,
+                minutes: workingHours.from.minutes } };
+        let timeAfterClosing = { to: { day: currentDay, hours: 23, minutes: 59 },
+            from: { day: currentDay, hours: workingHours.to.hours,
+                minutes: workingHours.to.minutes } };
+        moments.push(timeBeforeOpening, timeAfterClosing);
+    }
+
+    return moments;
+}
+
+function unionTimeIntervals(timeIntervals) {
+    let resultIntervals = [];
+    resultIntervals.push(timeIntervals[0]);
+
+    for (let i = 1; i < timeIntervals.length; i++) {
+        let currentInterval = timeIntervals[i];
+        let lastAdded = resultIntervals[resultIntervals.length - 1];
+        if (dateTime.equals(lastAdded.from, currentInterval.from) &&
+        dateTime.equals(lastAdded.to, currentInterval.to)) {
+            continue;
+        }
+
+        if (dateTime.compareDatetimes(lastAdded.to, currentInterval.from) > 0) {
+            lastAdded.to = dateTime.compareDatetimes(currentInterval.to, lastAdded.to) > 0
+                ? currentInterval.to
+                : lastAdded.to;
+        } else {
+            resultIntervals.push(currentInterval);
+        }
+    }
+
+    return resultIntervals;
+}
+
+function getAppropriateMoments(busyMoments, robberyDuration) {
+    let appropriateMoments = [];
+    for (let i = 0; i < busyMoments.length - 1; i++) {
+        let freeInterval = { from: busyMoments[i].to, to: busyMoments[i + 1].from };
+        if (getMomentDurationInMinutes(freeInterval) >= robberyDuration) {
+            appropriateMoments.push(freeInterval);
+        }
+    }
+
+    return appropriateMoments;
+}
+
+function getMomentDurationInMinutes(timeInterval) {
+    return dateTime.getElapsedMinutes(timeInterval.from, timeInterval.to);
+}
