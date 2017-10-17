b7d9e6c92cdffa1a80cc0d5d87ab583ea5fc5261
Ilya Kulygin <ruserixsian@gmail.com>
2017-10-17 18:33:56 +0500

    rewrite logic

--- robbery.js
+++ robbery.js
@@ -1,50 +1,132 @@
 'use strict';
 
-/**
- * Сделано задание на звездочку
- * Реализовано оба метода и tryLater
- */
-exports.isStar = true;
+const DATE_FORMAT = /(ПН|ВТ|СР)\s(\d\d):(\d\d)\+(\d)/;
+const DAYS = { 'ПН': 1, 'ВТ': 2, 'СР': 3 };
+const MILLIS_OF_MIN = 60000;
+const TRY_COOLDOWN = 30 * MILLIS_OF_MIN;
 
-/**
- * @param {Object} schedule – Расписание Банды
- * @param {Number} duration - Время на ограбление в минутах
- * @param {Object} workingHours – Время работы банка
- * @param {String} workingHours.from – Время открытия, например, "10:00+5"
- * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
- * @returns {Object}
- */
-exports.getAppropriateMoment = function (schedule, duration, workingHours) {
-    console.info(schedule, duration, workingHours);
+function parseDate(date) {
+    const [, day, hours, minutes, timezone] = date.match(DATE_FORMAT);
 
-    return {
+    return Date.UTC(2017, 4, DAYS[day], hours - timezone, minutes);
+}
 
-        /**
-         * Найдено ли время
-         * @returns {Boolean}
-         */
-        exists: function () {
-            return false;
-        },
+class AppropriateMoment {
 
-        /**
-         * Возвращает отформатированную строку с часами для ограбления
-         * Например,
-         *   "Начинаем в %HH:%MM (%DD)" -> "Начинаем в 14:59 (СР)"
-         * @param {String} template
-         * @returns {String}
-         */
-        format: function (template) {
-            return template;
-        },
+    /**
+     * @param {Object} schedule – Расписание Банды
+     * @param {Number} duration - Время на ограбление в минутах
+     * @param {Object} workingHours – Время работы банка
+     * @param {String} workingHours.from – Время открытия, например, "10:00+5"
+     * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
+     */
+    constructor(schedule, duration, workingHours) {
+        const bankSchedule = Object.keys(DAYS).map(day => ({
+            from: parseDate(`${day} ${workingHours.from}`),
+            to: parseDate(`${day} ${workingHours.to}`)
+        }));
 
-        /**
-         * Попробовать найти часы для ограбления позже [*]
-         * @star
-         * @returns {Boolean}
-         */
-        tryLater: function () {
-            return false;
+        const gangSchedule = this._mergeTimelines(Object.values(schedule)
+            .map(robber => robber.map(t => ({
+                from: parseDate(t.from),
+                to: parseDate(t.to)
+            })))
+        );
+
+        duration = duration * MILLIS_OF_MIN;
+
+        this._moments = bankSchedule.reduce((result, workDay) => {
+            for (let from = workDay.from; from <= workDay.to - duration; from += TRY_COOLDOWN) {
+                const candidate = { from, to: from + duration };
+
+                if (!gangSchedule.some((timeline) => this._areIntersected(timeline, candidate))) {
+                    result.push(candidate.from);
+                }
+            }
+
+            return result;
+        }, []).reverse();
+
+        this._moment = this._moments.pop();
+        this._bankTimezone = Number(workingHours.to.split('+')[1]);
+    }
+
+    /**
+     * Найдено ли время
+     * @returns {Boolean}
+     */
+    exists() {
+        return Boolean(this._moment);
+    }
+
+    /**
+     * Возвращает отформатированную строку с часами для ограбления
+     * Например,
+     *   "Начинаем в %HH:%MM (%DD)" -> "Начинаем в 14:59 (СР)"
+     * @param {String} template
+     * @returns {String}
+     */
+    format(template) {
+        if (!this.exists()) {
+            return '';
         }
-    };
-};
+
+
+        const offsetInMillis = this._bankTimezone * 60 * MILLIS_OF_MIN;
+        const date = new Date(this._moment + offsetInMillis);
+
+        return template
+            .replace('%HH', date.getUTCHours())
+            .replace('%MM', String(date.getUTCMinutes()).padStart(2, '0'))
+            .replace('%DD', Object.keys(DAYS)[date.getUTCDay() - 1]);
+    }
+
+    /**
+     * Попробовать найти часы для ограбления позже [*]
+     * @star
+     * @returns {Boolean}
+     */
+    tryLater() {
+        if (this.exists() && this._moments.length !== 0) {
+            this._moment = this._moments.pop();
+
+            return Boolean(this._moment);
+        }
+
+        return false;
+    }
+
+    /**
+     * Объединяет пересекающиеся временные отрезки
+     * @param {Array} timelines
+     * @returns {Array} 
+     */
+    _mergeTimelines(timelines) {
+        return timelines.slice()
+            .sort((a, b) => a.from - b.from)
+            .reduce((result, timeline) => {
+                const last = result[result.lenght - 1];
+                if (!last || !this._areIntersected(last, timeline)) {
+                    return result.concat(timeline);
+                }
+
+                last.to = Math.max(last.to, timeline.to);
+
+                return result;
+            }, []);
+    }
+
+    /**
+     * Пересекаются ли временные отрезки
+     * @param {Object} timeline1 
+     * @param {Object} timeline2
+     * @returns {Boolean}
+     */
+    _areIntersected(timeline1, timeline2) {
+        return timeline1.from < timeline2.to && timeline1.to > timeline2.from;
+    }
+}
+
+exports.isStar = true;
+exports.getAppropriateMoment = (schedule, duration, workingHours) =>
+    new AppropriateMoment(schedule, duration, workingHours);
