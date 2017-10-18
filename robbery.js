93eefe0ae0da55aa846ef2c338aa5c8d3c198555
Ludmilishe <luda_babich@mail.ru>
2017-10-18 21:59:00 +0500

    Тесты(1)

--- robbery.js
+++ robbery.js
@@ -19,10 +19,10 @@ function convertToHours(mins) {
     var hours = parseInt(mins / 60);
     var minutes = mins % 60;
     if (minutes < 10) {
-        minutes += '0';
+        minutes = '0' + minutes;
     }
     if (hours < 10) {
-        hours += '0';
+        hours = '0' + hours;
     }
 
     return hours + ':' + minutes;
