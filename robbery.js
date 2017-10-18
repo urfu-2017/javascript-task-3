a4abdef4c119bc05d1a9c9bf73f755c4a4153d2e
Naturin <bontortue@gmail.com>
2017-10-19 00:36:49 +0500

    v.1

--- robbery.js
+++ robbery.js
@@ -14,8 +14,24 @@ exports.isStar = true;
  * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
  * @returns {Object}
  */
+var GTM_BANK = 0;
+var WEEK_DAYS = ['ПН', 'ВТ', 'СР'];
+var MINUTES_IN_HOUR = 60;
+var HOUR_IN_DAY = 24;
+var TOTAL_TIME_LINE = WEEK_DAYS.length * HOUR_IN_DAY * MINUTES_IN_HOUR;
+var TRY_LATER_MINUTES = 30;
+var DURATION = 0;
+
+
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
-    console.info(schedule, duration, workingHours);
+    GTM_BANK = workingHours.from.split('+')[1];
+    DURATION = duration;
+
+    let gangPartyFreeTime = calcGangPartyFreeTime(schedule);
+    let bankFreeTime = getBankWorkingTime(workingHours);
+    gangPartyFreeTime.push(bankFreeTime);
+    let timeToGang = findTimeToGang(gangPartyFreeTime);
+    var currentGang = 0;
 
     return {
 
@@ -24,7 +40,7 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            return false;
+            return Boolean(timeToGang[currentGang]);
         },
 
         /**
@@ -35,7 +51,20 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            return template;
+            if (!timeToGang[currentGang]) {
+                return '';
+            }
+            let minutes = timeToGang[currentGang][0];
+            let day = WEEK_DAYS[(minutes - minutes % (HOUR_IN_DAY * MINUTES_IN_HOUR)) /
+                                                        (HOUR_IN_DAY * MINUTES_IN_HOUR)];
+            minutes %= HOUR_IN_DAY * MINUTES_IN_HOUR;
+            let hours = ((minutes - minutes % MINUTES_IN_HOUR) / MINUTES_IN_HOUR).toString();
+            minutes %= MINUTES_IN_HOUR;
+            minutes = minutes === 0 ? '00' : minutes;
+
+            return template.replace('%DD', day)
+                .replace('%HH', hours)
+                .replace('%MM', minutes);
         },
 
         /**
@@ -44,7 +73,105 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
-            return false;
+            var newTimeStart = timeToGang[currentGang][0] + TRY_LATER_MINUTES;
+            if ((timeToGang[currentGang][1] - newTimeStart) < duration) {
+                if (!timeToGang[currentGang + 1]) {
+                    return false;
+                }
+                currentGang++;
+
+                return true;
+            }
+            timeToGang[currentGang][0] += 30;
+
+            return true;
         }
     };
 };
+
+function calcGangPartyFreeTime(scheduleArr) {
+    var result = [];
+    for (let personName in scheduleArr) {
+        if (!scheduleArr.hasOwnProperty(personName)) {
+            continue;
+        }
+        var personResult = [];
+        for (let i = 0; i < scheduleArr[personName].length; i++) {
+            let start = stringToInt(scheduleArr[personName][i].from);
+            let end = stringToInt(scheduleArr[personName][i].to);
+            personResult.push([start, end]);
+        }
+        result.push(busyTimeToFreeTime(personResult));
+    }
+
+    return result;
+}
+
+function stringToInt(time) {
+    let format = time.match(/([А-Я]{2})\s(\d{2}):(\d{2})\+(\d+)/);
+    let hours = WEEK_DAYS.indexOf(format[1]) * HOUR_IN_DAY;
+    let delta = GTM_BANK - Number(format[4]);
+    hours += Number(format[2]) + delta;
+
+    return hours * MINUTES_IN_HOUR + Number(format[3]);
+}
+
+function busyTimeToFreeTime(timeArr) {
+    var result = [];
+    for (let i = 0; i <= timeArr.length; i++) {
+        if (i === 0) {
+            result.push([0, timeArr[i][0]]);
+            continue;
+        }
+        let end = timeArr[i] === undefined ? TOTAL_TIME_LINE : timeArr[i][0];
+        result.push([timeArr[i - 1][1], end]);
+    }
+
+    return result;
+}
+
+function getBankWorkingTime(timeArr) {
+    var result = [];
+    for (let i = 0; i < WEEK_DAYS.length; i++) {
+        result.push([stringToInt(WEEK_DAYS[i] + ' ' + timeArr.from),
+            stringToInt(WEEK_DAYS[i] + ' ' + timeArr.to)]);
+    }
+
+    return result;
+}
+
+function findTimeToGang(arrays, compare) {
+    var newArr = [];
+    var arr1 = compare ? compare : arrays[0];
+    var arr2 = compare ? arrays[0] : arrays[1];
+    for (let i = 0; i < arr1.length; i++) {
+        for (let k = 0; k < arr2.length; k++) {
+            // if (arr1[i][0] >= arr2[k][1] || arr1[i][1] <= arr2[k][0]) {
+            //     continue;
+            // }
+            // var el = [Math.max(arr1[i][0], arr2[k][0]), Math.min(arr1[i][1], arr2[k][1])];
+            // if ((el[1] - el[0]) >= duration) {
+            //     newArr.push(el);
+            // }
+            checkTimeInterval(arr1[i], arr2[k], newArr);
+        }
+    }
+    if (newArr.length && arrays[1]) {
+        arrays.shift();
+        newArr = findTimeToGang(arrays, newArr);
+    }
+
+    return newArr;
+}
+
+function checkTimeInterval(arr1, arr2, newArr) {
+    if (arr1[0] >= arr2[1] || arr1[1] <= arr2[0]) {
+        return;
+    }
+    var el = [Math.max(arr1[0], arr2[0]), Math.min(arr1[1], arr2[1])];
+    if ((el[1] - el[0]) >= DURATION) {
+        newArr.push(el);
+    }
+}
+
+
