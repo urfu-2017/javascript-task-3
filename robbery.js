0cd2a7f2e2d9b07170708c2c40cf334a816ad719
reindeermur <sofiabochik@gmail.com>
2017-10-19 20:45:14 +0500

    Update robbery.js

--- robbery.js
+++ robbery.js
@@ -15,6 +15,79 @@ const DAYS = {
     'СР': MIN_IN_DAY * 3
 };
 
+/**
+ * @param {Object} schedule – Расписание Банды
+ * @param {Number} duration - Время на ограбление в минутах
+ * @param {Object} workingHours – Время работы банка
+ * @param {String} workingHours.from – Время открытия, например, "10:00+5"
+ * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
+ * @returns {Object}
+ */
+exports.getAppropriateMoment = function (schedule, duration, workingHours) {
+    //console.info(schedule, duration, workingHours);
+    var TIME_ZONE = getTimeZone(workingHours.from);
+    var bankWorkingHours = getTimeInterval(workingHours);
+    var gangSchedule = getWorkingSchedule(schedule, TIME_ZONE);
+    var possibleStarts = tryToGetTime(gangSchedule, bankWorkingHours, duration);
+    
+    var timeForCrime = formatList(createTimeList(possibleStarts, duration));
+    var lastChance = timeForCrime.slice(-1);
+    
+
+    function changeTemplate(template) {
+        let time;
+        if (isEmpty()) {
+            time = lastChance;
+        } else {
+            time = timeForCrime[0];
+            timeForCrime.splice(0, 1);
+        }
+        return time;
+    }
+
+    function isEmpty() {
+        return timeForCrime.length === 0;
+    }
+
+    return {
+
+        /**
+         * Найдено ли время
+         * @returns {Boolean}
+         */
+        exists: function () {
+            return true;
+        },
+
+        /**
+         * Возвращает отформатированную строку с часами для ограбления
+         * Например,
+         *   "Начинаем в %HH:%MM (%DD)" -> "Начинаем в 14:59 (СР)"
+         * @param {String} template
+         * @returns {String}
+         */
+        format: function (template) {
+            let time = changeTemplate(template);
+            if (!isEmpty()) {
+                return template
+                    .replace('%HH', time[1])
+                    .replace('%MM', time[2])
+                    .replace('%DD', time[0]);
+            }
+            return '';
+        },
+
+        /**
+         * Попробовать найти часы для ограбления позже [*]
+         * @star
+         * @returns {Boolean}
+         */
+        tryLater: function () {
+            return (!isEmpty);
+        }
+    };
+};
+
 function getTimeZone(time) {
     return parseInt((time.slice(6)));
 }
@@ -27,11 +100,11 @@ function getHours(time) {
     return parseInt(time.slice(0, 2));
 }
 
-function convertToMinutes(time) {
+function convertToMinutes(time, timeZone) {
     let minutes = 0;
     minutes += getHours(time) * MIN_IN_HOUR;
     minutes += getMinutes(time);
-    minutes += (workingZone - getTimeZone(time)) * MIN_IN_HOUR;
+    minutes += (timeZone - getTimeZone(time)) * MIN_IN_HOUR;
 
     return minutes;
 }
@@ -47,12 +120,12 @@ function getDay(day) {
     return 0;
 }
 
-function getTimeInterval(record) {
+function getTimeInterval(record, timeZone) {
     let interval = [];
     let startTime = (record.from).split(' ');
     let endTime = (record.to).split(' ');
-    let start = convertToMinutes(startTime[1]);
-    let end = convertToMinutes(endTime[1]);
+    let start = convertToMinutes(startTime[1], timeZone);
+    let end = convertToMinutes(endTime[1], timeZone);
     start += getDay(startTime[0]);
     end += getDay(endTime[0]);
     interval.push(start);
@@ -65,27 +138,19 @@ function compareElements(a, b) {
     return a[0] - b[0];
 }
 
-function getSchedule() {
+function getSchedule(schedule, timeZone) {
     let reversedSchedule = [];
     for (let man of Object.keys(schedule)) {
         for (let record of schedule[man]) {
-            reversedSchedule.push(getTimeInterval(record));
+            reversedSchedule.push(getTimeInterval(record, timeZone));
         }
     }
 
     return reversedSchedule.sort(compareElements);
 }
 
-function getMax(a, b) {
-    if (a > b) {
-        return a;
-    }
-
-    return b;
-}
-
-function getWorkingSchedule() {
-    let oldSchedule = getSchedule();
+function getWorkingSchedule(schedule, timeZone) {
+    let oldSchedule = getSchedule(schedule, timeZone);
     let resultSchedule = [];
     if (oldSchedule[0][0] >= 0) {
         resultSchedule.push([0, oldSchedule[0][0]]);
@@ -105,6 +170,18 @@ function getWorkingSchedule() {
 
     return resultSchedule;
 
+}
+
+function tryToGetTime(gangSchedule, bankSchedule, duration) {
+    var startCrime = [];
+    for (let i = 0; i < gangSchedule.length; i++) {
+        for (let j = 0; j < bankSchedule.length; j++) {
+            startCrime.push(checkInterval(
+                gangSchedule[i], bankSchedule[j], duration));
+        }
+    }
+
+    return startCrime;
 }
 
 function getMin(a, b) {
@@ -157,17 +234,37 @@ function checkInterval(gang, bank, min) {
     return result;
 }
 
-function tryToGetTime() {
-    let gangSchedule = getWorkingSchedule();
-    var startCrime = [];
-    for (let i = 0; i < gangSchedule.length; i++) {
-        for (let j = 0; j < bankSchedule.length; j++) {
-            startCrime.push(checkInterval(
-                gangSchedule[i], bankSchedule[j], duration));
+function check(records) {
+    if (records.length !== 0) {
+        return true;
+    }
+
+    return false;
+}
+
+function showList(records, duration) {
+    let timelist = [];;
+    let n;
+    for (let i = 0; i < records.length; i++) {
+        n = (records[i][0]);
+        while ((n < records[i][1]) &&
+        (records[i][1] - n >= duration)) {
+            timelist.push(n);
+            n += 30;
+            n += duration;
         }
     }
 
-    return startCrime;
+    return timelist;
+}
+
+function createTimeList(records, duration) {
+    let timelist = [];
+    if (check(records)) {
+        timelist = showList(records, duration);
+    }
+
+    return timelist;
 }
 
 function transform(number) {
@@ -180,6 +277,7 @@ function transform(number) {
 
     return result;
 }
+
 function changeFormate(number) {
     let time = [];
     let oldTime = number;
@@ -196,116 +294,15 @@ function changeFormate(number) {
     let hours = (oldTime - (oldTime % MIN_IN_HOUR)) / MIN_IN_HOUR;
     let minutes = oldTime % MIN_IN_HOUR;
     time.push(transform (hours));
-    time.pudh(transform(minutes));
+    time.push(transform(minutes));
 
     return time;
 }
 
-function check() {
-    if (tryToGetTime().length !== 0) {
-        return true;
+function formatList(timeList) {
+    let result = [];
+    for (let i = 0; i < timeList.length; i++) {
+        result.push(changeFormate(timeList[i]));
     }
-
-    return false;
+    return result;
 }
-
-function showList() {
-    let timelist = [];
-    let timeInterval = tryToGetTime();
-    let n;
-    for (let i = 0; i < timeInterval.length; i++) {
-        n = (timeInterval[i][0]);
-        while ((n < timeInterval[i][1]) &&
-        (timeInterval[i][1] - n >= duration)) {
-            timelist.push(n);
-            n += 30;
-            n += duration;
-        }
-    }
-
-    return timelist;
-}
-
-function createTimeList() {
-    let timelist = [];
-    if (check()) {
-        timelist = showList();
-    }
-
-    return timelist;
-}
-
-function changeTemplate(template) {
-    let time;
-    if (topList.length === 0) {
-        time = changeFormate(lastChance);
-    } else {
-        time = changeFormate(topList[0]);
-        topList.splice(0, 1);
-    }
-
-    return template
-        .replace('%HH', time[1])
-        .replace('%MM', time[2])
-        .replace('%DD', time[0]);
-}
-
-/**
- * @param {Object} schedule – Расписание Банды
- * @param {Number} duration - Время на ограбление в минутах
- * @param {Object} workingHours – Время работы банка
- * @param {String} workingHours.from – Время открытия, например, "10:00+5"
- * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
- * @returns {Object}
- */
-exports.getAppropriateMoment = function (schedule, duration, workingHours) {
-    //console.info(schedule, duration, workingHours);
-    var workingZone = getTimeZone(workingHours.from);
-    var startBank = convertToMinutes(workingHours.from);
-    var endBank = convertToMinutes(workingHours.to);
-    
-    var bankSchedule = [
-        [startBank, endBank],
-        [startBank + DAYS.ПН, endBank + DAYS.ПН],
-        [startBank + DAYS.ВТ, endBank + DAYS.ВТ]
-    ];
-    var topList = createTimeList();
-    const lastChance = parseInt(topList.slice(-1));
-    var ok = check();
-    var letter = changeTemplate(template);
-    
-    return {
-
-        /**
-         * Найдено ли время
-         * @returns {Boolean}
-         */
-        exists: function () {
-            return ok;
-        },
-
-        /**
-         * Возвращает отформатированную строку с часами для ограбления
-         * Например,
-         *   "Начинаем в %HH:%MM (%DD)" -> "Начинаем в 14:59 (СР)"
-         * @param {String} template
-         * @returns {String}
-         */
-        format: function (template) {
-            if (ok) {
-                return (letter);
-            }
-            
-            return '';
-        },
-
-        /**
-         * Попробовать найти часы для ограбления позже [*]
-         * @star
-         * @returns {Boolean}
-         */
-        tryLater: function () {
-            return (topList.length === 0);
-        }
-    };
-};
