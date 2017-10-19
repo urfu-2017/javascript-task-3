35780d6c0ba17fa1ab6b3817612d3d94c0e3183a
evgenymarkov <evgenymarkov@jetstyle.ru>
2017-10-19 15:03:29 +0500

    Revert back star to true

--- robbery.js
+++ robbery.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализовано оба метода и tryLater
  */
-exports.isStar = false;
+exports.isStar = true;
 
 const TIME_REGEX = /^(\d{2}):(\d{2})\+(\d+)$/;
 const BANK_ROBBERY_DAYS = ['ПН', 'ВТ', 'СР'];
