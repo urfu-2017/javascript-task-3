12d6bd912559fefcdec8953dd3b52e584b92e090
reindeermur <sofiabochik@gmail.com>
2017-10-19 14:41:12 +0500

    rr

--- robbery.js
+++ robbery.js
@@ -15,6 +15,241 @@ const DAYS = {
     'СР': MIN_IN_DAY * 3
 };
 
+function getTimeZone(time) {
+    return parseInt((time.slice(6)));
+}
+
+function getMinutes(time) {
+    return parseInt(time.slice(3, 5));
+}
+
+function getHours(time) {
+    return parseInt(time.slice(0, 2));
+}
+
+function convertToMinutes(time) {
+    let minutes = 0;
+    minutes += getHours(time) * MIN_IN_HOUR;
+    minutes += getMinutes(time);
+    minutes += (workingZone - getTimeZone(time)) * MIN_IN_HOUR;
+
+    return minutes;
+}
+
+function getDay(day) {
+    if (day === 'ВТ') {
+        return (DAYS.ПН);
+    }
+    if (day === 'СР') {
+        return (DAYS.ВТ);
+    }
+
+    return 0;
+}
+
+function getTimeInterval(record) {
+    let interval = [];
+    let startTime = (record.from).split(' ');
+    let endTime = (record.to).split(' ');
+    let start = convertToMinutes(startTime[1]);
+    let end = convertToMinutes(endTime[1]);
+    start += getDay(startTime[0]);
+    end += getDay(endTime[0]);
+    interval.push(start);
+    interval.push(end);
+
+    return interval;
+}
+
+function compareElements(a, b) {
+    return a[0] - b[0];
+}
+
+function getSchedule() {
+    let reversedSchedule = [];
+    for (let man of Object.keys(schedule)) {
+        for (let record of schedule[man]) {
+            reversedSchedule.push(getTimeInterval(record));
+        }
+    }
+
+    return reversedSchedule.sort(compareElements);
+}
+
+function getMax(a, b) {
+    if (a > b) {
+        return a;
+    }
+
+    return b;
+}
+
+function getWorkingSchedule() {
+    let oldSchedule = getSchedule();
+    let resultSchedule = [];
+    if (oldSchedule[0][0] >= 0) {
+        resultSchedule.push([0, oldSchedule[0][0]]);
+    }
+    let currentMax = oldSchedule[0][1];
+    for (let i = 1; i < oldSchedule.length; i++) {
+        if (oldSchedule[i][0] + 1 <= currentMax) {
+            currentMax = getMax(currentMax, oldSchedule[i][1]);
+        } else {
+            resultSchedule.push([currentMax, oldSchedule[i][0]]);
+            currentMax = oldSchedule[i][1];
+        }
+    }
+    if (currentMax < DAYS.СР) {
+        resultSchedule.push([currentMax, DAYS.СР]);
+    }
+
+    return resultSchedule;
+
+}
+
+function getMin(a, b) {
+    if (a < b) {
+        return a;
+    }
+
+    return b;
+}
+
+function checkLeft(a, b, c, d) {
+    let result = [];
+    if ((a <= c) && (b > c)) {
+        if (((b < d) && (b - c >= duration)) ||
+        ((b >= d) && (d - c >= duration))) {
+
+            result.push(c);
+            result.push(getMin(d, b));
+        }
+    }
+
+    return result;
+}
+
+function checkRight(a, b, c, d) {
+    let result = [];
+    if ((c <= a) && (d > a)) {
+        if (((b < d) && (b - a >= duration)) ||
+        ((b >= d) && (d - a >= duration))) {
+
+            result.push(a);
+            result.push(getMin(d, b));
+        }
+    }
+
+    return result;
+}
+
+function checkInterval(gang, bank, min) {
+    let result = [];
+    let left = checkLeft(gang[0], gang[1], bank[0], bank[1], min);
+    let right = checkRight(gang[0], gang[1], bank[0], bank[1], min);
+    if (left.length !== 0) {
+        result = left;
+    }
+    if (right.length !== 0) {
+        result = right;
+    }
+
+    return result;
+}
+
+function tryToGetTime() {
+    let gangSchedule = getWorkingSchedule();
+    var startCrime = [];
+    for (let i = 0; i < gangSchedule.length; i++) {
+        for (let j = 0; j < bankSchedule.length; j++) {
+            startCrime.push(checkInterval(
+                gangSchedule[i], bankSchedule[j], duration));
+        }
+    }
+
+    return startCrime;
+}
+
+function transform(number) {
+    let result = '';
+    if (number < 10) {
+        result = ('0' + number);
+    } else {
+        result = number;
+    }
+
+    return result;
+}
+function changeFormate(number) {
+    let time = [];
+    let oldTime = number;
+    if (oldTime > DAYS.ВТ && oldTime <= DAYS.СР) {
+        time.push('СР');
+        oldTime -= DAYS.ВТ;
+    }
+    if (oldTime > DAYS.ПН && oldTime <= DAYS.ВТ) {
+        time.push('ВТ');
+        oldTime -= DAYS.ПН;
+    } else {
+        time.push('ПН');
+    }
+    let hours = (oldTime - (oldTime % MIN_IN_HOUR)) / MIN_IN_HOUR;
+    let minutes = oldTime % MIN_IN_HOUR;
+    time.push(transform (hours));
+    time.pudh(transform(minutes));
+
+    return time;
+}
+
+function check() {
+    if (tryToGetTime().length !== 0) {
+        return true;
+    }
+
+    return false;
+}
+
+function showList() {
+    let timelist = [];
+    let timeInterval = tryToGetTime();
+    let n;
+    for (let i = 0; i < timeInterval.length; i++) {
+        n = (timeInterval[i][0]);
+        while ((n < timeInterval[i][1]) &&
+        (timeInterval[i][1] - n >= duration)) {
+            timelist.push(n);
+            n += 30;
+            n += duration;
+        }
+    }
+
+    return timelist;
+}
+
+function createTimeList() {
+    let timelist = [];
+    if (check()) {
+        timelist = showList();
+    }
+
+    return timelist;
+}
+
+function changeTemplate(template) {
+    let time;
+    if (topList.length === 0) {
+        time = changeFormate(lastChance);
+    } else {
+        time = changeFormate(topList[0]);
+        topList.splice(0, 1);
+    }
+
+    return template
+        .replace('%HH', time[1])
+        .replace('%MM', time[2])
+        .replace('%DD', time[0]);
+}
+
 /**
  * @param {Object} schedule – Расписание Банды
  * @param {Number} duration - Время на ограбление в минутах
@@ -24,257 +259,21 @@ const DAYS = {
  * @returns {Object}
  */
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
-    console.info(schedule, duration, workingHours);
-
-    function getTimeZone(time) {
-        return parseInt((time.slice(6)));
-    }
-
-    function getMinutes(time) {
-        return parseInt(time.slice(3, 5));
-    }
-
-    function getHours(time) {
-        return parseInt(time.slice(0, 2));
-    }
-
+    //console.info(schedule, duration, workingHours);
     var workingZone = getTimeZone(workingHours.from);
-
-    function convertToMinutes(time) {
-        let minutes = 0;
-        minutes += getHours(time) * MIN_IN_HOUR;
-        minutes += getMinutes(time);
-        minutes += (workingZone - getTimeZone(time)) * MIN_IN_HOUR;
-
-        return minutes;
-    }
-
-    function getDay(day) {
-        if (day === 'ВТ') {
-            return (DAYS.ПН);
-        }
-        if (day === 'СР') {
-            return (DAYS.ВТ);
-        }
-
-        return 0;
-    }
-
-    function getTimeInterval(record) {
-        let interval = [];
-        let startTime = (record.from).split(' ');
-        let endTime = (record.to).split(' ');
-        let start = convertToMinutes(startTime[1]);
-        let end = convertToMinutes(endTime[1]);
-        start += getDay(startTime[0]);
-        end += getDay(endTime[0]);
-        interval.push(start);
-        interval.push(end);
-
-        return interval;
-    }
-
-    function compareElements(a, b) {
-        return a[0] - b[0];
-    }
-
-    function getSchedule() {
-        let reversedSchedule = [];
-        for (let man of Object.keys(schedule)) {
-            for (let record of schedule[man]) {
-                reversedSchedule.push(getTimeInterval(record));
-            }
-        }
-
-        return reversedSchedule.sort(compareElements);
-    }
-
-    function getMax(a, b) {
-        if (a > b) {
-            return a;
-        }
-
-        return b;
-    }
-
-    function getWorkingSchedule() {
-        let oldSchedule = getSchedule();
-        let resultSchedule = [];
-        if (oldSchedule[0][0] >= 0) {
-            resultSchedule.push([0, oldSchedule[0][0]]);
-        }
-        let currentMax = oldSchedule[0][1];
-        for (let i = 1; i < oldSchedule.length; i++) {
-            if (oldSchedule[i][0] + 1 <= currentMax) {
-                currentMax = getMax(currentMax, oldSchedule[i][1]);
-            } else {
-                resultSchedule.push([currentMax, oldSchedule[i][0]]);
-                currentMax = oldSchedule[i][1];
-            }
-        }
-        if (currentMax < DAYS.СР) {
-            resultSchedule.push([currentMax, DAYS.СР]);
-        }
-
-        return resultSchedule;
-
-    }
-
     var startBank = convertToMinutes(workingHours.from);
     var endBank = convertToMinutes(workingHours.to);
-
+    
     var bankSchedule = [
         [startBank, endBank],
         [startBank + DAYS.ПН, endBank + DAYS.ПН],
         [startBank + DAYS.ВТ, endBank + DAYS.ВТ]
     ];
-
-    function getMin(a, b) {
-        if (a < b) {
-            return a;
-        }
-
-        return b;
-    }
-
-    function checkLeft(a, b, c, d) {
-        let result = [];
-        if ((a <= c) && (b > c)) {
-            if (((b < d) && (b - c >= duration)) ||
-            ((b >= d) && (d - c >= duration))) {
-
-                result.push(c);
-                result.push(getMin(d, b));
-            }
-        }
-
-        return result;
-    }
-
-    function checkRight(a, b, c, d) {
-        let result = [];
-        if ((c <= a) && (d > a)) {
-            if (((b < d) && (b - a >= duration)) ||
-            ((b >= d) && (d - a >= duration))) {
-
-                result.push(a);
-                result.push(getMin(d, b));
-            }
-        }
-
-        return result;
-    }
-
-    function checkInterval(gang, bank, min) {
-        let result = [];
-        let left = checkLeft(gang[0], gang[1], bank[0], bank[1], min);
-        let right = checkRight(gang[0], gang[1], bank[0], bank[1], min);
-        if (left.length !== 0) {
-            result = left;
-        }
-        if (right.length !== 0) {
-            result = right;
-        }
-
-        return result;
-    }
-
-    function tryToGetTime() {
-        let gangSchedule = getWorkingSchedule();
-        var startCrime = [];
-        for (let i = 0; i < gangSchedule.length; i++) {
-            for (let j = 0; j < bankSchedule.length; j++) {
-                startCrime.push(checkInterval(
-                    gangSchedule[i], bankSchedule[j], duration));
-            }
-        }
-
-        return startCrime;
-    }
-
-    function transform(number) {
-        let result = '';
-        if (number < 10) {
-            result = ('0' + number);
-        } else {
-            result = number;
-        }
-
-        return result;
-    }
-    function changeFormate(number) {
-        let time = [];
-        let oldTime = number;
-        if (oldTime > DAYS.ВТ && oldTime <= DAYS.СР) {
-            time.push('СР');
-            oldTime -= DAYS.ВТ;
-        }
-        if (oldTime > DAYS.ПН && oldTime <= DAYS.ВТ) {
-            time.push('ВТ');
-            oldTime -= DAYS.ПН;
-        } else {
-            time.push('ПН');
-        }
-        let hours = (oldTime - (oldTime % MIN_IN_HOUR)) / MIN_IN_HOUR;
-        let minutes = oldTime % MIN_IN_HOUR;
-        time.push(transform (hours));
-        time.pudh(transform(minutes));
-
-        return time;
-    }
-
-    function check() {
-        if (tryToGetTime().length !== 0) {
-            return true;
-        }
-
-        return false;
-    }
-
-    function showList() {
-        let timelist = [];
-        let timeInterval = tryToGetTime();
-        let n;
-        for (let i = 0; i < timeInterval.length; i++) {
-            n = (timeInterval[i][0]);
-            while ((n < timeInterval[i][1]) &&
-            (timeInterval[i][1] - n >= duration)) {
-                timelist.push(n);
-                n += 30;
-                n += duration;
-            }
-        }
-
-        return timelist;
-    }
-
-    function createTimeList() {
-        let timelist = [];
-        if (check()) {
-            timelist = showList();
-        }
-
-        return timelist;
-    }
-
     var topList = createTimeList();
     const lastChance = parseInt(topList.slice(-1));
-
-    function changeTemplate(template) {
-        let time;
-        if (topList.length === 0) {
-            time = changeFormate(lastChance);
-        } else {
-            time = changeFormate(topList[0]);
-            topList.splice(0, 1);
-        }
-
-        return template
-            .replace('%HH', time[1])
-            .replace('%MM', time[2])
-            .replace('%DD', time[0]);
-    }
-
+    var ok = check();
+    var letter = changeTemplate(template);
+    
     return {
 
         /**
@@ -282,7 +281,7 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            return check();
+            return ok;
         },
 
         /**
@@ -293,8 +292,8 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            if (check()) {
-                return (changeTemplate(template));
+            if (ok) {
+                return (letter);
             }
             
             return '';
