f63265d092da2bdd36ae4c23ec9dbe9bdc7ed345
danmerus <leoghost@mail.ru>
2017-10-19 11:30:13 +0500

    First try

--- robbery.js
+++ robbery.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализовано оба метода и tryLater
  */
-exports.isStar = true;
+exports.isStar = false;
 
 /**
  * @param {Object} schedule – Расписание Банды
@@ -14,8 +14,206 @@ exports.isStar = true;
  * @param {String} workingHours.to – Время закрытия, например, "18:00+5"
  * @returns {Object}
  */
+
+function formDict(name) {
+    var day = []
+    for (var i = 0; i < name.length; i++) {
+        var t1 = toMinsFromMonday(name[i].from);
+        var t2 = toMinsFromMonday(name[i].to);
+        day.push([t1, t2]);
+    }
+
+    return day;
+
+}
+function findIntersection(inter1, inter2) {
+    if (inter1 === [] || inter2 === []) {
+        return [];
+    }
+    var t1 = inter1[0];
+    var t2 = inter1[1];
+    var t3 = inter2[0];
+    var t4 = inter2[1];
+    if (t1 >= t3 && t2 <= t4) { //внутри
+        return [t1, t2];
+    }
+    if (t2 <= t3) { //ниже
+        return [];
+    }
+    if (t1 >= t4) { //выше
+        return [];
+    }
+    if (t1 <= t3 && t2 >= t4) { //снаружи
+        return [t3, t4];
+    }
+    if (t2 >= t4 && t1 <= t4 && t1 >= t3) {
+        return [t1,t4];
+    }
+    if (t1 <= t3 && t2 <= t4 && t2 >= t3) {
+        return [t3, t2];
+    }
+    return [];
+}
+function trickSystem(period, arr) {
+    if (period === []) {
+        return;
+    }
+    var out = [];
+    for (var i = 0; i < arr.length; i++) {
+        out.push(findIntersection(arr[i], period));
+    }
+    return out;
+}
+function toMinsFromMonday(date) {
+    var res = 0;
+    var day = date.split(' ')[0];
+    var time = date.split(' ')[1].substring(0, 5);
+    var zone = date.split(' ')[1].substring(6);
+    var hours = time.split(':')[0];
+    var mins = time.split(':')[1];
+    switch(day) {
+        case 'ПН':
+            res += 0;
+            break;
+        case 'ВТ':
+            res += 1440;
+            break;
+        case 'СР':
+            res += 2880;
+            break;
+        default:
+            break;
+    }
+    res += (parseInt(hours)-parseInt(zone)) * 60 + parseInt(mins);
+
+    return res;
+}
+function minsFromMonToDay(mins, bankZone) {
+    mins += bankZone * 60;
+    var d = ~~(mins / 1440);
+    var h = ~~((mins - (d * 1440)) / 60);
+    var m = mins - (d * 1440) - (h * 60);
+    h = h.toString();
+    m = m.toString();
+    if (m.length < 2) {
+        m = '0' + m;
+    }
+    if (h.length < 2) {
+        h = '0' + h;
+    }
+    switch(d) {
+        case 0:
+            return ['ПН', h + ':' + m];
+        case 1:
+            return ['ВТ', h + ':' + m];
+        case 2:
+            return ['СР', h + ':' + m];
+        default:
+            return;
+    }
+}
+function getBank(bank) {
+    var out = [];
+    var zone = parseInt(bank.from.substring(6));
+    var open = bank.from.substring(0, 5);
+    var close = bank.to.substring(0, 5);
+    var hOpens = open.split(':')[0];
+    var mOpens = open.split(':')[1];
+    var hClosed = close.split(':')[0];
+    var mClosed = close.split(':')[1];
+    var Op = (parseInt(hOpens) - zone) * 60 + parseInt(mOpens);
+    var Clo = (parseInt(hClosed) - zone) * 60 + parseInt(mClosed);
+    out.push([Op, Clo], [Op + 1440, Clo + 1440], [Op + 2880, Clo + 2880]);
+
+    return out;
+}
+function getFreeTimeIntervals(intervals) {
+    var start = Number.NEGATIVE_INFINITY;
+    var out = [];
+    for (var i = 0; i < intervals.length; i++) {
+        out.push([start, intervals[i][0]]);
+        start = intervals[i][1];
+    }
+    if (parseInt(start) < 4320) {
+        out.push([start, 4320]);
+    }
+
+    return out;
+}
+function cleanUp(match) {
+    var temp = [];
+    var out = [];
+    var t = match[0].length;
+    for (var i = 0; i < match.length; i++) {
+        for (var j = 0; j < t; j++) {
+            temp.push(match[i][j]);
+        }
+    }
+    for ( i = 0; i < temp.length; i++) {
+        if (temp[i].length > 0) {
+            out.push(temp[i]);
+        }
+    }
+
+    return out;
+}
+function bankMatches(robb, bank) {
+    var out = [];
+    for (var i = 0; i < robb.length; i++) {
+        for (var j = 0; j < bank.length; j++) {
+            out.push(findIntersection(robb[i], bank[j]));
+        }
+    }
+
+    return out;
+}
+function timesForRobbery(results) {
+    var out = [];
+    for (var i = 0; i < results.length; i++) {
+        out.push(results[i][1] - results[i][0]);
+    }
+
+    return out;
+}
+function getRightTime(schedule, duration, workingHours) {
+    var danny = [];
+    var rusty = [];
+    var linus = [];
+    var bank = [];
+    bank = getBank(workingHours);
+    danny = formDict(schedule.Danny);
+    rusty = formDict(schedule.Rusty);
+    linus = formDict(schedule.Linus);
+    danny = getFreeTimeIntervals(danny);
+    rusty = getFreeTimeIntervals(rusty);
+    linus = getFreeTimeIntervals(linus);
+    var out = [];
+    for (var i = 0; i < danny.length; i++) {
+        for (var j = 0; j < rusty.length; j++) {
+            var res = findIntersection(danny[i], rusty[j]);
+            out.push(trickSystem(res, linus));
+        }
+    }
+    out = cleanUp(out);
+    var result = bankMatches(out, bank);
+    var cleanRes = [];
+    for (i = 0; i < result.length; i++) {
+        if (result[i].length > 0) {
+            cleanRes.push(result[i]);
+        }
+    }
+    var times = timesForRobbery(cleanRes);
+    var time = -1;
+    for (i = 0; i < times.length; i++) {
+        if (times[i] >= duration) {
+            time = cleanRes[i];
+            return time;
+        }
+    }
+    return time;
+}
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
-    console.info(schedule, duration, workingHours);
+   // console.info(schedule, duration, workingHours);
 
     return {
 
@@ -24,7 +222,42 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            return false;
+            var danny = [];
+            var rusty = [];
+            var linus = [];
+            var bank = [];
+            bank = getBank(workingHours);
+            danny = formDict(schedule.Danny);
+            rusty = formDict(schedule.Rusty);
+            linus = formDict(schedule.Linus);
+            danny = getFreeTimeIntervals(danny);
+            rusty = getFreeTimeIntervals(rusty);
+            linus = getFreeTimeIntervals(linus);
+            var out = [];
+            for (var i = 0; i < danny.length; i++) {
+                for (var j = 0; j < rusty.length; j++) {
+                    var res = findIntersection(danny[i], rusty[j]);
+                    out.push(trickSystem(res, linus));
+                }
+            }
+            out = cleanUp(out);
+            var result = bankMatches(out, bank);
+            var cleanRes = [];
+            for (i = 0; i < result.length; i++) {
+                if (result[i].length > 0) {
+                    cleanRes.push(result[i]);
+                }
+            }
+            var times = timesForRobbery(cleanRes);
+           // console.log(times);
+            var exists = false;
+            for (i = 0; i < times.length; i++) {
+                if (times[i] >= duration) {
+                    exists = true;
+                }
+            }
+            
+            return exists;
         },
 
         /**
@@ -35,6 +268,19 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
+            if (getRightTime(schedule, duration, workingHours) === -1) {
+                return '';
+            }
+            var time = getRightTime(schedule, duration, workingHours);
+            var zone = parseInt(workingHours.from.substring(6));
+            var parsedStart = minsFromMonToDay(time[0],zone);
+            var d = parsedStart[0];
+            var h = parsedStart[1].split(':')[0];
+            var m = parsedStart[1].split(':')[1];
+            template = template.replace('%DD', d);
+            template = template.replace('%HH', h);
+            template = template.replace('%MM', m);
+
             return template;
         },
 
