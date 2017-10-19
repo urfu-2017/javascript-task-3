d211ab948d56d6bb6f57e5b0b41eaf47c68a94f0
Anton Mukhinsky <antonn.aka.fly@gmail.com>
2017-10-19 12:05:55 +0500

    done main task

--- robbery.js
+++ robbery.js
@@ -4,7 +4,90 @@
  * Сделано задание на звездочку
  * Реализовано оба метода и tryLater
  */
-exports.isStar = true;
+exports.isStar = false;
+var dateRegex = /([А-Я]{2})\s(\d{2}):(\d{2})\+(\d{1,2})/;
+var days = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+var daysForRob = days.slice(0, 3);
+
+function getBankTimeTable(workingHours) {
+    let timeTable = [];
+    daysForRob.forEach(function (day) {
+        let dateTimeFrom = parseDateString(day + ' ' + workingHours.from);
+        let dateTimeTo = parseDateString(day + ' ' + workingHours.to);
+        timeTable.push({
+            from: dateTimeFrom,
+            to: dateTimeTo
+        });
+    });
+
+    return timeTable;
+}
+
+function parseDateString(dateString) {
+    let parcedDateString = dateString.match(dateRegex);
+    let day = days.indexOf(parcedDateString[1]) + 1;
+    let timezone = Number(parcedDateString[4]);
+    let hours = Number(parcedDateString[2]) - timezone;
+    let minutes = Number(parcedDateString[3]);
+    let dateTime = new Date(2017, 10, day, hours, minutes);
+
+    return dateTime;
+}
+
+function performIntersectIntervals(schedule) {
+    schedule.sort(function (first, second) {
+
+        return first.from.getTime() - second.from.getTime();
+    });
+    let performedSchedule = [schedule[0]];
+    schedule = schedule.slice(1);
+    while (schedule.length !== 0) {
+        let curInter = schedule[0];
+        let isIntersect = false;
+        performedSchedule.forEach(function (perfomInter) {
+            if (curInter.from <= perfomInter.to && curInter.to >= perfomInter.from) {
+                perfomInter.to = perfomInter.to >= curInter.to ? perfomInter.to : curInter.to;
+                isIntersect = true;
+            }
+        });
+        if (!isIntersect) {
+            performedSchedule.push(curInter);
+        }
+        schedule = schedule.slice(1);
+    }
+
+    return performedSchedule;
+}
+
+function getRobTime(bankTimetable, schedule) {
+    let robTimes = [];
+    bankTimetable.forEach(function (workday) {
+        schedule.forEach(function (interval) {
+            if (workday.from <= interval.to && workday.to >= interval.from) {
+                let robTime = {
+                    from: workday.from >= interval.from ? workday.from : interval.from,
+                    to: workday.to >= interval.to ? interval.to : workday.to
+                };
+                robTimes.push(robTime);
+            }
+        });
+    });
+
+    return robTimes;
+}
+
+function inverseIntervals(schedule, bankTimezone) {
+    let from = parseDateString('ПН 00:00+' + bankTimezone);
+    let to = parseDateString('СР 23:59+' + bankTimezone);
+    let inversed = [{ from, to: schedule[0].from }];
+    for (let i = 0; i <= schedule.length - 2; i++) {
+        inversed.push({ from: schedule[i].to, to: schedule[i + 1].from });
+    }
+    inversed.push({ from: schedule[schedule.length - 1].to, to });
+
+    return inversed;
+}
+
 
 /**
  * @param {Object} schedule – Расписание Банды
@@ -15,7 +98,9 @@ exports.isStar = true;
  * @returns {Object}
  */
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
-    console.info(schedule, duration, workingHours);
+    // console.info(schedule, duration, workingHours);
+    let robTimes;
+    let bankTimezone = ('ПН ' + workingHours.from).match(dateRegex)[4];
 
     return {
 
@@ -24,7 +109,34 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            return false;
+            let names = Object.keys(schedule);
+            let parsedSchedule = [];
+            names.forEach(function (name) {
+                schedule[name].forEach(function (namedSchedule) {
+                    parsedSchedule.push({
+                        from: parseDateString(namedSchedule.from),
+                        to: parseDateString(namedSchedule.to)
+                    });
+
+                });
+            }
+            );
+
+
+            let out = performIntersectIntervals(parsedSchedule);
+
+            let timeTable = getBankTimeTable(workingHours);
+
+            let inversed = inverseIntervals(out, bankTimezone);
+
+            robTimes = getRobTime(timeTable, inversed);
+            robTimes = robTimes.filter(function (time) {
+                let timeToRob = (time.to - time.from) / 60000;
+
+                return duration <= timeToRob;
+            });
+
+            return robTimes.length > 0;
         },
 
         /**
@@ -35,7 +147,19 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            return template;
+            if (robTimes.length === 0) {
+
+                return '';
+            }
+            let time = robTimes[0].from;
+            time.setTime(time.getTime() + bankTimezone * 60 * 60 * 1000);
+            let day = days[time.getDate() - 1];
+            let hours = time.getHours();
+            let minutes = time.getMinutes();
+
+            return template.replace('%DD', day)
+                .replace('%HH', hours)
+                .replace('%MM', minutes);
         },
 
         /**
@@ -44,6 +168,7 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
+
             return false;
         }
     };
