1c91453be2ad1879955ccf76187cb154d20afe5d
ValeryZhurba <valera.zhurba@gmail.com>
2017-10-20 13:27:17 +0400

    test3

--- robbery.js
+++ robbery.js
@@ -172,9 +172,7 @@ function getBankSchedule(workingHours) {
 
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
 
-    /* const bankSchedule = getBankSchedule(workingHours);
-    const bankUTCZone = bankSchedule[0].from.UTCZone;
-    const generalSchedule = getSchedule(schedule, bankUTCZone);
+    /*
     let freeTimeGeneralSchedule = [];
     Object.keys(generalSchedule).forEach(function (name) {
         freeTimeGeneralSchedule.push(getFreeTime(generalSchedule[name], bankUTCZone));
@@ -189,12 +187,13 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
     const DannyFreeTime = getFreeTime(generalSchedule.Danny, bankUTCZone);
     const LinusFreeTime = getFreeTime(generalSchedule.Linus, bankUTCZone);
     const RastyFreeTime = getFreeTime(generalSchedule.Rusty, bankUTCZone);
-    let intersectionSchedule =
-        getIntersectionSchedule(DannyFreeTime, LinusFreeTime, bankUTCZone);
-    intersectionSchedule =
-        getIntersectionSchedule(intersectionSchedule, RastyFreeTime, bankUTCZone);
-    intersectionSchedule =
-        getIntersectionSchedule(intersectionSchedule, bankSchedule, bankUTCZone);
+    let freeTimeGeneralSchedule = [];
+    freeTimeGeneralSchedule.push(DannyFreeTime);
+    freeTimeGeneralSchedule.push(LinusFreeTime);
+    freeTimeGeneralSchedule.push(RastyFreeTime);
+    freeTimeGeneralSchedule.push(bankSchedule);
+    let intersectionSchedule = freeTimeGeneralSchedule.reduce((prev, curr) =>
+        getIntersectionSchedule(prev, curr, bankUTCZone));
     const appropriateMoment = intersectionSchedule.filter(function (value) {
         return (value.to.timeInMinutes - value.from.timeInMinutes) >= duration;
     });
