99342d33e4aa901d9b9e123436e99682ffd580a1
fng3r <dinvk@mail.ru>
2017-10-23 00:04:42 +0500

    code review' fixes

--- datetime-utils.js
+++ datetime-utils.js
@@ -3,12 +3,10 @@
 const MILLISECONDS_IN_MINUTE = 60 * 1000;
 const MINUTES_IN_HOUR = 60;
 
-exports.createDate = function createDate(day, hours, minutes, utcOffset = 0) {
+exports.createDate = (day, hours, minutes, utcOffset = 0) => {
     //  June, 1970 starts with Monday
     return new Date(1970, 5, day, hours - utcOffset, minutes);
 };
-
-exports.formatTime = time => String(time).padStart(2, '0');
 
 exports.addMinutesToDate = (date, minutes) => {
     return new Date(date.getTime() + minutesToMilliseconds(minutes));
@@ -23,3 +21,5 @@ exports.minutesToMilliseconds = minutesToMilliseconds;
 exports.hoursToMinutes = hours => hours * MINUTES_IN_HOUR;
 
 exports.hoursToMilliseconds = hours => hours * MINUTES_IN_HOUR * MILLISECONDS_IN_MINUTE;
+
+exports.formatTime = time => String(time).padStart(2, '0');
