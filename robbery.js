0b15c63302263273e54e28ed936c58541ea11c3f
GalinaRoma <kisaga@mail.ru>
2017-10-23 23:13:24 +0500

    Вынесла несколько констант; Изменила название двух методов(deleteWrongDays и niteSchedule) и одной переменной(globalDiff); Убрала лишнее условие при обходе всех дней до даления неподходящих; Упростила два одинаковых if; Убрала опцию useGrouping в toLocaleString; В parseInt() указала систему счисления; Расположила функции в том порядке, в котором они вызываются в коде.

--- robbery.js
+++ robbery.js
@@ -6,6 +6,10 @@
  */
 exports.isStar = false;
 
+let BANK_TIME_ZONE;
+let WEEK_DAYS = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
+let DURATION;
+
 /**
  * @param {Object} schedule – Расписание Банды
  * @param {Number} duration - Время на ограбление в минутах
@@ -15,19 +19,20 @@ exports.isStar = false;
  * @returns {Object}
  */
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
-    let bankTimezone = getTimezone(workingHours.from);
+    BANK_TIME_ZONE = getTimezone(workingHours.from);
+    DURATION = duration;
     let bandBusyTime = { 'ВС': [], 'ПН': [], 'ВТ': [], 'СР': [], 'ЧТ': [], 'ПТ': [], 'СБ': [] };
     for (let busyTimeList of Object.values(schedule)) {
         for (let currentBusyTime of busyTimeList) {
-            createGeneralSchedule(bandBusyTime, currentBusyTime, bankTimezone);
+            createGeneralSchedule(bandBusyTime, currentBusyTime);
         }
     }
-    deleteWrongDays(bandBusyTime);
+    deleteUnsuitableDays(bandBusyTime);
     let timesForRob = { 'ПН': [], 'ВТ': [], 'СР': [] };
     for (let day of Object.keys(bandBusyTime)) {
         let scheduleInWorkHours = cutSchedule(bandBusyTime[day], workingHours);
         let sortedSchedule = scheduleInWorkHours.sort(sortSchedule);
-        let modifiedSchedule = uniteSchedule(sortedSchedule);
+        let modifiedSchedule = uniteTimeSegments(sortedSchedule);
         timesForRob[day] = findSuitableTime(modifiedSchedule, duration, workingHours);
     }
 
@@ -111,16 +116,13 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
     };
 };
 
-function deleteWrongDays(bandBusyTime) {
-    delete bandBusyTime['ВС'];
-    delete bandBusyTime['ЧТ'];
-    delete bandBusyTime['ПТ'];
-    delete bandBusyTime['СБ'];
+function getTimezone(time) {
+    return parseInt(time.split('+')[1], 10);
 }
 
-function createGeneralSchedule(bandBusyTime, currentBusyTime, bankTimezone) {
-    let from = transformTime(currentBusyTime.from, bankTimezone);
-    let to = transformTime(currentBusyTime.to, bankTimezone);
+function createGeneralSchedule(bandBusyTime, currentBusyTime) {
+    let from = transformTime(currentBusyTime.from);
+    let to = transformTime(currentBusyTime.to);
     if (from.day === to.day) {
         if (from.day in bandBusyTime) {
             bandBusyTime[from.day].push({ from: from.time, to: to.time });
@@ -131,71 +133,55 @@ function createGeneralSchedule(bandBusyTime, currentBusyTime, bankTimezone) {
     }
 }
 
+function transformTime(dayAndTime) {
+    let [day, hours, minutes, currentTimezone] = dayAndTime.split(/[ :+]/);
+    let diff = BANK_TIME_ZONE - currentTimezone;
+    let dayShift = 0;
+    hours = parseInt(hours, 10) + diff;
+    if (hours < 0) {
+        hours += 24;
+        dayShift = -1;
+    }
+    if (hours >= 24) {
+        hours %= 24;
+        dayShift = +1;
+    }
+    day = getWeekDay(day, dayShift);
+    hours = hours.toLocaleString('en-US', { minimumIntegerDigits: 2 });
+
+    return { day, time: `${hours}:${minutes}` };
+}
+
+function getWeekDay(currentDay, shift) {
+    let currentIndex = WEEK_DAYS.indexOf(currentDay);
+
+    return WEEK_DAYS[currentIndex + shift];
+}
+
+function divideIntoDays(from, to) {
+    let segmentByDays = {};
+    let startDay = WEEK_DAYS.indexOf(from.day);
+    let endDay = WEEK_DAYS.indexOf(to.day);
+    segmentByDays[from.day] = { from: from.time, to: '23:59' };
+    segmentByDays[to.day] = { from: '00:00', to: to.time };
+    for (let i = startDay + 1; i < endDay; i++) {
+        segmentByDays[WEEK_DAYS[i]] = { from: '00:00', to: '23:59' };
+    }
+
+    return segmentByDays;
+}
+
 function addToGeneralSchedule(busyDays, bandBusyTime) {
     for (let day of Object.keys(busyDays)) {
-        if (day in bandBusyTime) {
-            bandBusyTime[day].push(busyDays[day]);
-        }
+        bandBusyTime[day].push(busyDays[day]);
     }
 }
 
-function sumTime(first, second) {
-    let [hourFirst, minuteFirst] = first.split(':');
-    let [hourSecond, minuteSecond] = second.split(':');
-    let minuteSum = parseInt(minuteSecond) + parseInt(minuteFirst);
-    let hourSum = parseInt(hourSecond) + parseInt(hourFirst);
-    if (minuteSum >= 60) {
-        hourSum += 1;
-        minuteSum %= 60;
-    }
-
-    return hourSum.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) +
-         ':' + minuteSum.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
-}
-
-function findSuitableTime(schedule, duration, workingHours) {
-    let robTime = [];
-    let startHour = workingHours.from.split('+')[0];
-    let endHour = workingHours.to.split('+')[0];
-    let globalDiff = diffTime(startHour, endHour);
-    if (schedule.length === 0 && globalDiff >= duration) {
-        return [{ start: startHour, freeTime: globalDiff }];
-    } else if (schedule.length === 0) {
-        return [];
-    }
-    let diffWithStart = diffTime(startHour, schedule[0].from);
-    if (diffWithStart >= duration) {
-        robTime.push({ start: startHour, freeTime: diffWithStart });
-    }
-    addFreeSegments(schedule, robTime, duration);
-    let diffWithEnd = diffTime(schedule[schedule.length - 1].to, endHour);
-    if (diffWithEnd >= duration) {
-        robTime.push({ start: schedule[schedule.length - 1].to, freeTime: diffWithEnd });
-    }
-
-    return robTime;
-}
-
-function addFreeSegments(schedule, robTime, duration) {
-    for (let i = 0; i < schedule.length - 1; i++) {
-        let diff = diffTime(schedule[i].to, schedule[i + 1].from);
-        if (diff >= duration) {
-            robTime.push({ start: schedule[i].to, freeTime: diff });
-        }
-    }
-}
-
-function diffTime(first, second) {
-    let [hourFirst, minuteFirst] = first.split(':');
-    let [hourSecond, minuteSecond] = second.split(':');
-    let minuteDiff = parseInt(minuteSecond) - parseInt(minuteFirst);
-    let hourDiff = parseInt(hourSecond) - parseInt(hourFirst);
-    if (minuteDiff < 0) {
-        hourDiff -= 1;
-        minuteDiff = 60 + minuteDiff;
-    }
-
-    return hourDiff * 60 + minuteDiff;
+function deleteUnsuitableDays(bandBusyTime) {
+    delete bandBusyTime['ВС'];
+    delete bandBusyTime['ЧТ'];
+    delete bandBusyTime['ПТ'];
+    delete bandBusyTime['СБ'];
 }
 
 function cutSchedule(schedule, workingHours) {
@@ -239,7 +225,7 @@ function sortSchedule(first, second) {
     return result;
 }
 
-function uniteSchedule(schedule) {
+function uniteTimeSegments(schedule) {
     let counter = 0;
     while (counter < schedule.length - 1) {
         if (schedule[counter].to >= schedule[counter + 1].from) {
@@ -273,47 +259,71 @@ function minTime(first, second) {
     return min;
 }
 
-function divideIntoDays(from, to) {
-    let weekDays = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
-    let segmentByDays = {};
-    let startDay = weekDays.indexOf(from.day);
-    let endDay = weekDays.indexOf(to.day);
-    segmentByDays[from.day] = { from: from.time, to: '23:59' };
-    segmentByDays[to.day] = { from: '00:00', to: to.time };
-    for (let i = startDay + 1; i < endDay; i++) {
-        segmentByDays[weekDays[i]] = { from: '00:00', to: '23:59' };
-    }
+function findSuitableTime(schedule, duration, workingHours) {
+    let robTime = [];
+    let startHour = workingHours.from.split('+')[0];
+    let endHour = workingHours.to.split('+')[0];
+    let workingHoursAmount = diffTime(startHour, endHour);
+    if (schedule.length === 0) {
+        if (workingHoursAmount >= duration) {
+            return [{ start: startHour, freeTime: workingHoursAmount }];
+        }
 
-    return segmentByDays;
+        return [];
+    }
+    let diffWithStart = diffTime(startHour, schedule[0].from);
+    compareStartDiffWithDuration(diffWithStart, robTime, startHour);
+    addFreeSegments(schedule, robTime, duration);
+    let diffWithEnd = diffTime(schedule[schedule.length - 1].to, endHour);
+    compareEndDiffWithDuration(diffWithEnd, robTime, schedule);
+
+    return robTime;
 }
 
-function transformTime(dayAndTime, bankTimezone) {
-    let [day, hours, minutes, currentTimezone] = dayAndTime.split(/[ :+]/);
-    let diff = bankTimezone - currentTimezone;
-    let dayShift = 0;
-    hours = parseInt(hours) + diff;
-    if (hours < 0) {
-        hours += 24;
-        dayShift = -1;
+function diffTime(first, second) {
+    let [hourFirst, minuteFirst] = first.split(':');
+    let [hourSecond, minuteSecond] = second.split(':');
+    let minuteDiff = parseInt(minuteSecond, 10) - parseInt(minuteFirst, 10);
+    let hourDiff = parseInt(hourSecond, 10) - parseInt(hourFirst, 10);
+    if (minuteDiff < 0) {
+        hourDiff -= 1;
+        minuteDiff = 60 + minuteDiff;
     }
-    if (hours >= 24) {
-        hours %= 24;
-        dayShift = +1;
-    }
-    day = getWeekDay(day, dayShift);
-    hours = hours.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
 
-    return { day, time: `${hours}:${minutes}` };
+    return hourDiff * 60 + minuteDiff;
 }
 
-function getWeekDay(currentDay, shift) {
-    let weekDays = ['ВС', 'ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ'];
-    let currentIndex = weekDays.indexOf(currentDay);
-
-    return weekDays[currentIndex + shift];
+function compareStartDiffWithDuration(diff, robTime, startHour) {
+    if (diff >= DURATION) {
+        robTime.push({ start: startHour, freeTime: diff });
+    }
 }
 
+function compareEndDiffWithDuration(diff, robTime, schedule) {
+    if (diff >= DURATION) {
+        robTime.push({ start: schedule[schedule.length - 1].to, freeTime: diff });
+    }
+}
 
-function getTimezone(time) {
-    return parseInt(time.split('+')[1]);
+function addFreeSegments(schedule, robTime, duration) {
+    for (let i = 0; i < schedule.length - 1; i++) {
+        let diff = diffTime(schedule[i].to, schedule[i + 1].from);
+        if (diff >= duration) {
+            robTime.push({ start: schedule[i].to, freeTime: diff });
+        }
+    }
+}
+
+function sumTime(first, second) {
+    let [hourFirst, minuteFirst] = first.split(':');
+    let [hourSecond, minuteSecond] = second.split(':');
+    let minuteSum = parseInt(minuteSecond, 10) + parseInt(minuteFirst, 10);
+    let hourSum = parseInt(hourSecond, 10) + parseInt(hourFirst, 10);
+    if (minuteSum >= 60) {
+        hourSum += 1;
+        minuteSum %= 60;
+    }
+
+    return hourSum.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false }) +
+         ':' + minuteSum.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false });
 }
