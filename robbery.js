15e42938963d0c814ce478b944a515a078532131
AlexBarett <iron.angel876527@gmail.com>
2017-12-15 22:20:09 +0500

    мне там баллов немного не хватает

--- robbery.js
+++ robbery.js
@@ -6,176 +6,100 @@
  */
 exports.isStar = true;
 
-const WEEK = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+const halfWeek = {
+    'ПН': 0,
+    'ВТ': 1,
+    'СР': 2,
+    0: 'ПН',
+    1: 'ВТ',
+    2: 'СР'
+};
+var minutesForRobbery = 3 * 24 * 60;
 
-function forming(day, hours, minutes) {
-    if (hours < 0) {
-        day = WEEK[WEEK.indexOf(day) - 1];
-        hours += 24;
-    }
-    if (hours > 23) {
-        day = WEEK[WEEK.indexOf(day) + 1];
-        hours -= 24;
-    }
-    if (hours < 10) {
-        hours = '0' + hours;
-    }
-    if (minutes < 10) {
-        minutes = '0' + parseInt(minutes);
-    }
+function toMinutes(time, shift) {
+    let minutes = time.split(' ');
+    minutes = (halfWeek[minutes[0]] * 24 + parseInt(minutes[1].split(':')[0]) + shift) * 60 +
+        parseInt(minutes[1].split(':')[1].slice(0, 2));
 
-    return day + ' ' + hours + ':' + minutes;
+    return minutes;
 }
 
-function timeSort(a, b) {
-    a = a.from;
-    b = b.from;
+function filterRobbers(availableMinutes, schedule, workingHours) {
+    let bankTimeZone = parseInt(workingHours.from.split('+')[1]);
+    let shift = bankTimeZone - parseInt(schedule.Danny[0].from.split('+')[1]);
 
-    return timeCompare(a, b);
-}
+    schedule.Danny.forEach(filterTimeSegment);
 
-function timeCompare(a, b) {
-    var [dayA, timeA] = a.split(' ');
-    var [dayB, timeB] = b.split(' ');
-    if (WEEK.indexOf(dayA) < WEEK.indexOf(dayB)) {
+    shift = bankTimeZone - parseInt(schedule.Rusty[0].from.split('+')[1]);
+    schedule.Rusty.forEach(filterTimeSegment);
 
-        return -1;
-    } else if (timeA < timeB && dayA === dayB) {
+    shift = bankTimeZone - parseInt(schedule.Linus[0].from.split('+')[1]);
+    schedule.Linus.forEach(filterTimeSegment);
 
-        return -1;
-    }
-    if (dayA === dayB && timeA === timeB) {
+    function filterTimeSegment(timeSegment) {
+        let start = timeSegment.from;
+        let finish = timeSegment.to;
 
-        return 0;
-    }
-    if (WEEK.indexOf(dayA) > WEEK.indexOf(dayB)) {
-
-        return 1;
-    } else if (timeA > timeB && dayA === dayB) {
-
-        return 1;
+        start = toMinutes(start, shift);
+        finish = toMinutes(finish, shift);
+        availableMinutes = availableMinutes.filter(time => !(time >= start && time < finish));
     }
 
-    return 1;
+    return availableMinutes;
 }
 
-function toOneTimeZone(schedule, workingHours) {
-    var oneTimeSchedule = [];
-    var bankTimeZone = parseInt(workingHours.from.split('+')[1]);
-    oneTimeSchedule = oneTimeSchedule.concat(schedule.Danny, schedule.Rusty, schedule.Linus);
-    oneTimeSchedule = oneTimeSchedule.map(day => {
-        var differenceTime = bankTimeZone - parseInt(day.from.split('+')[1]);
-        var [busyFrom, timeFrom] = day.from.split(' ');
-        var [busyTo, timeTo] = day.to.split(' ');
-        timeFrom = timeFrom.split(':');
-        timeTo = timeTo.split(':');
-        timeTo[0] = parseInt(timeTo[0]) + differenceTime;
-        timeFrom[0] = parseInt(timeFrom[0]) + differenceTime;
-        busyFrom = forming(busyFrom, timeFrom[0], timeFrom[1].slice(0, 2));
-        busyTo = forming(busyTo, timeTo[0], timeTo[1].slice(0, 2));
+function filterBunkCloseTime(availableMinutes, workingHours) {
+    let openMonday = toMinutes('ПН ' + workingHours.from, 0);
+    let openTuesday = toMinutes('ВТ ' + workingHours.from, 0);
+    let openWednesday = toMinutes('СР ' + workingHours.from, 0);
+    let closeMonday = toMinutes('ПН ' + workingHours.to, 0);
+    let closeTuesday = toMinutes('ВТ ' + workingHours.to, 0);
+    let closeWednesday = toMinutes('СР ' + workingHours.to, 0);
 
-        return { from: busyFrom, to: busyTo };
-    });
+    availableMinutes = availableMinutes.filter(time =>
+        ((time >= openMonday && time < closeMonday) ||
+        (time >= openTuesday && time < closeTuesday) ||
+        (time >= openWednesday && time < closeWednesday)));
 
-    return oneTimeSchedule.sort(timeSort);
+    return availableMinutes;
 }
 
-function combineTime(schedule) {
-    var busyTime = [];
-    var currentFrom = schedule[0].from;
-    var currentTo = schedule[0].to;
-    for (var i = 1; i < schedule.length; i++) {
-        if (timeCompare(currentTo, schedule[i].from) === -1 ||
-            timeCompare(currentTo, schedule[i].to) === 1) {
-            busyTime.push({ from: currentFrom, to: currentTo });
-            currentFrom = schedule[i].from;
+function combimeMinutes(availableMinutes) {
+    let startSegment = availableMinutes[0];
+    let freeTime = 1;
+    availableMinutes = availableMinutes.reduce(function (freeTimeSegments, currentTime, index) {
+        if (index !== 0) {
+            if (currentTime - availableMinutes[index - 1] === 1) {
+                freeTime ++;
+
+            } else {
+                freeTimeSegments.push({ start: startSegment, time: freeTime });
+                startSegment = currentTime;
+                freeTime = 1;
+            }
         }
-        currentTo = schedule[i].to;
+
+        return freeTimeSegments;
+    }, []);
+
+    return availableMinutes;
+}
+
+function toNormalTime(time) {
+    var normalTime = {};
+    normalTime.day = halfWeek[parseInt(time / 1440)];
+    time = time - parseInt(time / 1440) * 1440;
+    normalTime.hours = parseInt(time / 60);
+    if (normalTime.hours < 10) {
+        normalTime.hours = '0' + normalTime.hours;
     }
-    busyTime.push({ from: currentFrom, to: currentTo });
-
-    return busyTime;
-}
-
-function splitDays(schedule) {
-    var newSchedule = [];
-    for (var i = 0; i < schedule.length; i++) {
-        if (schedule[i].from.split(' ')[0] !== schedule[i].to.split(' ')[0]) {
-            newSchedule.push({ from: schedule[i].from, 
-                to: schedule[i].from.split(' ')[0] + ' 23:59' });
-            schedule[i] = { from: schedule[i].to.split(' ')[0] + ' 00:00', to: schedule[i].to };
-            i--;
-        } else {
-            newSchedule.push(schedule[i]);
-        }
-    }
-
-    return newSchedule;
-}
-
-function timecount(from, to) {
-    var [startHours, startMinutes] = from.split(' ')[1].split(':');
-    var [finishHours, finishMinutes] = to.split(' ')[1].split(':');
-    from = parseInt(startHours) * 60 + parseInt(startMinutes);
-    to = parseInt(finishHours) * 60 + parseInt(finishMinutes);
-
-    return to - from;
-}
-
-function add30Seconds(time) {
-    var [hours, minutes] = time.split(' ')[1].split(':');
-    var day = time.split(' ')[0];
-    minutes = parseInt(minutes) + 30;
-    if (minutes >= 60) {
-        hours = parseInt(hours) + 1;
-        minutes -= 60;
+    time = time - parseInt(time / 60) * 60;
+    normalTime.minutes = time;
+    if (normalTime.minutes < 10) {
+        normalTime.minutes = '0' + normalTime.minutes;
     }
 
-    return forming(day, hours, minutes);
-}
-
-function bankCompare(bank, time) {
-    bank = time.split(' ')[0] + ' ' + bank;
-
-    return timeCompare(bank, time);
-}
-
-function findMoment(schedule, workingHours) {
-    var avalibleTime = []
-    var start = workingHours.from.split('+')[0];
-    var finish = workingHours.to.split('+')[0];
-    for (var i = 0; i < schedule.length; i++) {
-        var day = schedule[i].from.split(' ')[0] + ' ';
-        if (bankCompare(start, schedule[i].to) === 1 ||
-            bankCompare(finish, schedule[i].from) === -1) {
-                avalibleTime.push({ from: day + start, to: day + finish });
-            }
-        if (bankCompare(start, schedule[i].from) === -1 &&
-            bankCompare(finish, schedule[i].to) === 1) {
-                avalibleTime.push({ from: day + start , to: schedule[i].from });
-                avalibleTime.push({ from: schedule[i].to , to: day + finish });
-            }
-        if (bankCompare(start, schedule[i].from) === -1 &&
-            bankCompare(finish, schedule[i].to) === -1 &&
-            bankCompare(finish, schedule[i].from) === 1) {
-                avalibleTime.push({ from: schedule[i].from, to: day + finish });
-            }
-        if (bankCompare(start, schedule[i].from) === 1 &&
-            bankCompare(finish, schedule[i].to) === 1) {
-                avalibleTime.push({ from: schedule[i].to , to: day + finish });
-            }
-    }
-
-    return concatTime(avalibleTime);
-}
-
-function concatTime(avalibleTime) {
-    var timeToGo = [];
-    var day = avalibleTime[0].from.split(' ')[0];
-    for(var i = 1; i < avalibleTime.length; i++) {
-        var currentDay = avalibleTime[i].from.split(' ')[0];
-        if ()
-    }
+    return normalTime;
 }
 
 /**
@@ -187,14 +111,14 @@ function concatTime(avalibleTime) {
  * @returns {Object}
  */
 exports.getAppropriateMoment = function (schedule, duration, workingHours) {
-    console.info(schedule, duration, workingHours);
-    var oneTimeSchedule = toOneTimeZone(schedule, workingHours);
-    var appropriateTime = splitDays(combineTime(oneTimeSchedule), workingHours);
-    console.info('--------------');
-    console.info(appropriateTime);
-    console.info('--------------');
-    console.info(findMoment(appropriateTime, workingHours));
-    var startTime = appropriateTime.find(moment => moment.time >= duration);
+    var availableMinutes = Array.from({ length: minutesForRobbery }, (v, k) => k);
+    availableMinutes = filterBunkCloseTime(availableMinutes, workingHours);
+    availableMinutes = filterRobbers(availableMinutes, schedule, workingHours);
+    availableMinutes = combimeMinutes(availableMinutes);
+    var beginingTime = availableMinutes.find(timeSegment => timeSegment.time >= duration);
+    if (beginingTime !== undefined) {
+        beginingTime = beginingTime.start;
+    }
 
     return {
 
@@ -203,13 +127,7 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         exists: function () {
-            if (startTime) {
-                startTime = startTime.from;
-
-                return true;
-            }
-
-            return false;
+            return (beginingTime !== undefined);
         },
 
         /**
@@ -220,12 +138,16 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {String}
          */
         format: function (template) {
-            if (startTime) {
-                template = 'Метим на ' + startTime.split(' ')[0] + ', старт в ' +
-                    startTime.split(' ')[1] + '!';
+            if (beginingTime !== undefined) {
+                let date = toNormalTime(beginingTime);
+
+                return template
+                    .replace('%DD', date.day)
+                    .replace('%HH', date.hours)
+                    .replace('%MM', date.minutes);
             }
 
-            return template;
+            return '';
         },
 
         /**
@@ -234,21 +156,20 @@ exports.getAppropriateMoment = function (schedule, duration, workingHours) {
          * @returns {Boolean}
          */
         tryLater: function () {
-            if (startTime) {
-                var newStartTime = add30Seconds(startTime);
-                var index = appropriateTime.findIndex(moment =>
-                    timeCompare(startTime, moment.to) === -1);
-                var newAppropriateTime = appropriateTime.slice(index);
-                newAppropriateTime[0] = { from: newStartTime, to: newAppropriateTime[0].to,
-                    time: timecount(newStartTime, newAppropriateTime[0].to) };
-                newStartTime = newAppropriateTime.find(moment => moment.time >= duration);
-                if (newStartTime) {
-                    startTime = newStartTime.from;
-
-                    return true;
+            if (beginingTime === undefined) {
+                return false;
+            }
+            let later = beginingTime + 30;
+            let index = availableMinutes.findIndex(time => (time.start >= later &&
+                time.time >= duration) || time.start + time.time >= duration + later);
+            if (index !== -1) {
+                if (beginingTime === availableMinutes[index].start) {
+                    beginingTime = later;
+                } else {
+                    beginingTime = availableMinutes[index].start;
                 }
 
-                return false;
+                return true;
             }
 
             return false;
